ab2a593f06739944ae4882d88c01d55bfdbce7c5
Sauli Anto <sauli.anto@gofore.com>
2015-11-26 11:02:47 +0200

    Tehty skripti jolla voi mankeloida bittikartat kuvista

---
+++ svg/batch_export.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Inkscape täytyy olla ympäristömuuttujissa
+# Kuvat skaalataan leveyden mukaan
+
+IKONIT="ikoni-eperusteet ikoni-haku ikoni-nuoli-alas ikoni-nuoli-oikealle"
+
+function render {
+        inkscape --file="$1.svg" --export-png="../png/$2/$1.png" --export-area-page --export-width=$3
+}
+
+for KUVA in $IKONIT; do
+    echo processing icon: ${FILE}.svg
+    render $KUVA "xs" 32
+    render $KUVA "sm" 64
+    render $KUVA "md" 128
+    render $KUVA "lg" 256
+done
