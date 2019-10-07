04708cbc3871ad288bb8625b28a4ff973419a0b8
Sauli Anto <sauli.anto@gmail.com>
2019-10-07 16:02:22 +0300

    Siirretty generaattori yhteisiin

---
+++ vue/src/api/eperusteet.ts
@@ -0,0 +1,43 @@
+import { Configuration } from '../generated/eperusteet';
+import axios, { AxiosInstance } from 'axios';
+import _ from 'lodash';
+import {
+  PerusteetApi,
+  TiedotteetApi,
+} from '../generated/eperusteet';
+
+const basePath = '';
+export const baseURL = '/eperusteet-service/api';
+
+const ax = axios.create({
+  baseURL,
+});
+
+function axiosHandler(msg: string) {
+  return async (err: any) => {
+    console.error(msg as any, err);
+    throw err;
+  };
+}
+
+function successfulResponseHandler() {
+  return async (res: any) => {
+    return res;
+  };
+}
+
+ax.interceptors.request.use(_.identity, axiosHandler('Request error'));
+ax.interceptors.response.use(successfulResponseHandler(), axiosHandler('Response error'));
+
+// https://github.com/Microsoft/TypeScript/issues/20719
+type BaseAPIConstructor<T> = new(configuration?: Configuration, basePath?: string, axios?: AxiosInstance) => T;
+
+const configuration = { basePath };
+
+function initApi<T>(X: BaseAPIConstructor<T>): T {
+  return new X(configuration, basePath, ax);
+}
+
+export const Api = ax;
+export const Perusteet = initApi(PerusteetApi);
+export const Tiedotteet = initApi(TiedotteetApi);
---
+++ vue/src/api/tyypit.ts
@@ -0,0 +1,7 @@
+import * as EperusteetApi from '../generated/eperusteet/api';
+import * as YlopsApi from '../generated/ylops/api';
+
+
+export type PerusteDto = EperusteetApi.PerusteDto;
+export type PerusteHakuDto = EperusteetApi.PerusteHakuDto;
+export type TiedoteDto = EperusteetApi.TiedoteDto;
