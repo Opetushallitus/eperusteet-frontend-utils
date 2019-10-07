61464d50548570d6a02e3952066473f6d4587118
Niko Junkala <niko.junkala@gofore.com>
2019-10-07 14:12:23 +0300

    Lisätty puuttuvat komponentit

---
+++ vue/src/components/EpSpinner/EpSpinnerInline.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.oph-spinner.oph-spinner-in-button
+  .oph-bounce.oph-bounce1
+  .oph-bounce.oph-bounce2
+  .oph-bounce.oph-bounce3
+</template>
+
+<style scoped lang="scss">
+.oph-spinner-in-button {
+  margin-left: 15px;
+}
+</style>
---
+++ vue/src/components/EpSpinner/EpSpinnerSlot.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="spinner">
+  <ep-spinner v-if="isLoading"/>
+  <slot v-else></slot>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator';
+import EpSpinner from './EpSpinner.vue';
+
+@Component({
+  components: {
+    EpSpinner,
+  },
+})
+export default class EpSpinnerSlot extends Vue {
+  @Prop({ required: true })
+  private isLoading!: boolean;
+}
+</script>
+
+<style>
+</style>
