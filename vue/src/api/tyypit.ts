eea349829a30d5452eb83cc2fc529a2f9e8ad655
Niko Junkala <niko.junkala@gofore.com>
2019-10-08 10:09:02 +0300

    Päivitetty tyyppejä

--- vue/src/api/tyypit.ts
+++ vue/src/api/tyypit.ts
@@ -5,3 +5,9 @@ import * as YlopsApi from '../generated/ylops/api';
 export type PerusteDto = EperusteetApi.PerusteDto;
 export type PerusteHakuDto = EperusteetApi.PerusteHakuDto;
 export type TiedoteDto = EperusteetApi.TiedoteDto;
+export type PageTiedoteDto = EperusteetApi.PageTiedoteDto;
+export type PerusteKoosteDto = EperusteetApi.PerusteKoosteDto;
+
+export type OpetussuunnitelmaDto = YlopsApi.OpetussuunnitelmaDto
+export type OpetussuunnitelmaInfoDto = YlopsApi.OpetussuunnitelmaInfoDto;
+export type OpetussuunnitelmaJulkinenDto = YlopsApi.OpetussuunnitelmaJulkinenDto;
