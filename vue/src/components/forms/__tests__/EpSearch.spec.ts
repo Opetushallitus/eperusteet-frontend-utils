804b78eb790bf2ec1acec7c416a1a1bcccfe270c
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-10-15 16:41:33 +0300

    Tests added

---
+++ vue/src/components/forms/__tests__/EpSearch.spec.ts
@@ -0,0 +1,65 @@
+import { mount, createLocalVue, shallowMount } from '@vue/test-utils';
+import EpSearch from '../EpSearch.vue';
+import { KieliStore } from '../../../stores/kieli';
+
+describe('EpFormContent component', () => {
+
+  const localVue = createLocalVue();
+
+  KieliStore.setup(localVue,{
+    messages: {
+      fi: {
+        'etsi': 'Etsi'
+      }
+    },
+  });
+
+  const i18n = KieliStore.i18n;
+
+  function mountWrapper(updateSearch, propsit) { 
+    return mount(localVue.extend({
+      components: {
+        EpSearch,
+      },
+      methods: {
+        updateSearch
+      },
+      data(){
+        return propsit;
+      },
+      template: '<ep-search v-model="rajain" @input="updateSearch" :placeholder="placeholder"/>'
+    }), {
+      localVue,
+      i18n,
+      stubs: {
+        fas: true
+      },
+    });
+  };
+
+  test('Renders content with content', async () => {
+    const testMethod = jest.fn();
+    const wrapper = mountWrapper(testMethod, {placeholder: null, rajain:''});
+    expect(wrapper.html()).toContain('Etsi');
+  });
+
+  test('Renders content with content and props', async () => {
+    const testMethod = jest.fn();
+    const wrapper = mountWrapper(testMethod, {placeholder: 'etsi-teksti', rajain:''});
+    wrapper.setProps({placeholder: 'etsi-teksti'});
+    await localVue.nextTick();
+    expect(wrapper.find('input').html()).toContain('etsi-teksti');
+  });
+
+  test('Renders content with content and props', async () => {
+    const testMethod = jest.fn();
+    const testrajain = '';
+    const wrapper = mountWrapper(testMethod, {placeholder: 'etsi-teksti', rajain: testrajain});
+
+    wrapper.find('input').setValue('arvoa');
+    await localVue.nextTick();
+    expect(testMethod).toBeCalled();
+
+  });
+
+});
