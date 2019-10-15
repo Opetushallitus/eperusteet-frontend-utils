320f5e82613960267ee7e353c24bc2d9ffb98593
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-10-15 09:51:03 +0300

    EP-1653: components tests

---
+++ vue/src/components/forms/__tests__/EpField.spec.ts
@@ -0,0 +1,21 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import EpField from '../EpField.vue';
+
+describe('EpInput component', () => {
+
+  const localVue = createLocalVue();
+
+  test('Renders field with content', async () => {
+    const wrapper = mount(EpField, {
+      propsData: {
+        value: 'arvo',
+      },
+      localVue
+    });
+
+    await localVue.nextTick();
+
+    expect(wrapper.html()).toContain('arvo');
+  });
+
+});
