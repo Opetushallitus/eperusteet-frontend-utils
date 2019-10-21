e350658902834ef77889c96bd0b5a3d2d57e4d9f
Sauli Anto <sauli.anto@gmail.com>
2019-10-21 22:18:57 +0300

    Ajettu lint

--- vue/src/tyypit.ts
+++ vue/src/tyypit.ts
@@ -14,26 +14,26 @@ export interface SovellusVirhe {
 }
 
 export enum Koulutustyyppi {
-  perustutkinto = "koulutustyyppi_1",
-  lukiokoulutus = "koulutustyyppi_2",
-  telma = "koulutustyyppi_5",
-  lisaopetus = "koulutustyyppi_6",
-  ammattitutkinto = "koulutustyyppi_11",
-  erikoisammattitutkinto = "koulutustyyppi_12",
-  aikuistenlukiokoulutus = "koulutustyyppi_14",
-  esiopetus = "koulutustyyppi_15",
-  perusopetus = "koulutustyyppi_16",
-  aikuistenperusopetus = "koulutustyyppi_17",
-  valma = "koulutustyyppi_18",
-  varhaiskasvatus = "koulutustyyppi_20",
-  perusopetusvalmistava = "koulutustyyppi_22",
-  lukiovalmistavakoulutus = "koulutustyyppi_23",
-  tpo = "koulutustyyppi_999907"
+  perustutkinto = 'koulutustyyppi_1',
+  lukiokoulutus = 'koulutustyyppi_2',
+  telma = 'koulutustyyppi_5',
+  lisaopetus = 'koulutustyyppi_6',
+  ammattitutkinto = 'koulutustyyppi_11',
+  erikoisammattitutkinto = 'koulutustyyppi_12',
+  aikuistenlukiokoulutus = 'koulutustyyppi_14',
+  esiopetus = 'koulutustyyppi_15',
+  perusopetus = 'koulutustyyppi_16',
+  aikuistenperusopetus = 'koulutustyyppi_17',
+  valma = 'koulutustyyppi_18',
+  varhaiskasvatus = 'koulutustyyppi_20',
+  perusopetusvalmistava = 'koulutustyyppi_22',
+  lukiovalmistavakoulutus = 'koulutustyyppi_23',
+  tpo = 'koulutustyyppi_999907'
 }
 
 export enum KoulutustyyppiToteutus {
-  yksinkertainen = "yksinkertainen",
-  perusopetus = "perusopetus",
-  lops = "lops",
-  lops2019 = "lops2019"
+  yksinkertainen = 'yksinkertainen',
+  perusopetus = 'perusopetus',
+  lops = 'lops',
+  lops2019 = 'lops2019'
 }
