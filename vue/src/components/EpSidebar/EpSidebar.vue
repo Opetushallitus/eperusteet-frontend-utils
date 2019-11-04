e97f797db7847979d6b149cc2eb4694c40fe7611
Sauli Anto <sauli.anto@gmail.com>
2019-11-04 13:53:22 +0200

    Päivitetty sidebaria

--- vue/src/components/EpSidebar/EpSidebar.vue
+++ vue/src/components/EpSidebar/EpSidebar.vue
@@ -1,27 +1,19 @@
 <template>
 <div class="sidenav">
-  <div class="bar" :class="{ 'bar-open': toggled}">
-    <div class="bar-buttons">
-      <div class="btn-group-sm" :class="{ 'btn-group': toggled, 'btn-group-vertical': !toggled }">
-        <button class="btn btn-link btn-sm"
-                :aria-label="$t('avaa-rakenteen-navigaatio')"
-                @click="toggled = !toggled">
-          <fas fixed-width icon="bars"></fas>
-        </button>
-        <button class="btn btn-link btn-sm"
-                :aria-label="$t('avaa-asetukset')"
-                id="popover-button-event">
-          <fas fixed-width icon="cog"></fas>
-        </button>
-
-        <b-popover target="popover-button-event" triggers="click blur">
-          <template v-slot:title>{{ $t('lisaasetukset') }}</template>
-          <ep-toggle v-model="autoScroll">{{ $t('automaattinen-nayton-vieritys')}}</ep-toggle>
-          <ep-toggle v-model="showSubchapter" :is-editing="false">{{ $t('avaa-paalukujen-aliluvut-automaattisesti')}}</ep-toggle>
-        </b-popover>
-      </div>
+  <div class="bar" :class="{ 'bar-open': isOpen}">
+    <div class="bar-buttons d-block">
+      <!-- Desktop -->
+      <ep-sidebar-buttons :mobile="false"
+                          :inline="isOpen"
+                          v-model="settings"
+                          @toggle="handleToggle" />
+      <!-- Mobile -->
+      <ep-sidebar-buttons :mobile="true"
+                          :inline="isOpen"
+                          v-model="settings"
+                          @toggle="handleToggle" />
     </div>
-    <slot v-if="toggled"
+    <slot v-if="isOpen"
           name="bar"></slot>
   </div>
   <div class="view" :id="scrollId">
@@ -33,47 +25,51 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import EpToggle from '../forms/EpToggle.vue';
-
+import EpSidebarButtons from './EpSidebarButtons.vue';
 @Component({
   components: {
     EpToggle,
+    EpSidebarButtons,
   }
 })
 export default class EpSidebar extends Vue {
-  private toggled = true;
-  private autoScroll = true;
-  private showSubchapter = true;
-
+  private isOpen = true;
+  private settings = {
+    autoScroll: true,
+    showSubchapter: true
+  };
   get scrollId() {
-    return this.autoScroll ? 'scroll-anchor' : 'scroll-anchor-disabled';
+    return this.settings.autoScroll ? 'scroll-anchor' : 'scroll-anchor-disabled';
+  }
+  private handleToggle(value) {
+    this.isOpen = value;
   }
 }
-
 </script>
 <style scoped lang="scss">
 @import "../../styles/_variables.scss";
-
 .sidenav {
   .bar {
     .bar-buttons {
       padding: 0 $content-padding;
     }
   }
-
+  @media (max-width: 767.98px) {
+    .btn-group-vertical {
+      flex-direction: row;
+    }
+  }
   @media (min-width: 768px) {
     display: flex;
-
     .bar {
       &.bar-open {
         width: $sidebar-width;
       }
     }
-
     .view {
       flex: 1;
       border-left: 1px solid #eee;
     }
   }
-
 }
 </style>
