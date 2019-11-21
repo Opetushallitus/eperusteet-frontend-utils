17bf4c849eedaad543665e72a81a7d1cfb0caba7
Sauli Anto <sauli.anto@gmail.com>
2019-11-21 10:24:47 +0200

    Lisätty teeman vaihdon apufunktio

---
+++ vue/src/utils/icon.ts
@@ -0,0 +1,51 @@
+import * as _ from 'lodash';
+import { createLogger } from "./logger";
+
+const logger = createLogger('Main');
+
+
+function changeIcons(isModeDark: boolean) {
+  const links = document.getElementsByTagName('link');
+  if (isModeDark) {
+    logger.info('Change dark mode');
+    _.each(links, link => {
+      if (link.hasAttribute('href') && _.includes(link.getAttribute('href'), 'favicon')) {
+        const href = _.replace(link.getAttribute('href')!, 'favicon','favicon-inverted');
+        link.setAttribute('href', href);
+      }
+    });
+  }
+  else {
+    logger.info('Change light mode');
+    _.each(links, link => {
+      if (link.hasAttribute('href') && _.includes(link.getAttribute('href'), 'favicon')) {
+        const href = _.replace(link.getAttribute('href')!, 'favicon-inverted','favicon');
+        link.setAttribute('href', href);
+      }
+    });
+  }
+}
+
+export function registerIconColorSchemeChange() {
+  if (_.isFunction(window.matchMedia)) {
+    const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Register listener
+    matcher.addEventListener('change', () => {
+      if (matcher.matches) {
+        changeIcons(true);
+      }
+      else {
+        changeIcons(false);
+      }
+    });
+
+    // Init state
+    if (matcher.matches) {
+      changeIcons(true);
+    }
+    else {
+      changeIcons(false);
+    }
+  }
+}
