9d550fa8653518349fc5de8fcd74e63f25a0b8f3
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-12-02 16:39:59 +0200

    EP-1918

---
+++ vue/src/components/EpAlert/EpAlert.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img :src="pictureSrc"/>
+    <span class="alert">{{ text }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class EpAlert extends Vue {
+  @Prop({ required: true })
+  private text!: string;
+
+  @Prop({ default: false, required: false })
+  private ops!: boolean;
+
+  get pictureSrc() {
+    return this.ops ? '../../../../img/icons/ops_tyhja-tila.svg' : '../../../../img/icons/tyhja-tila.svg';
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .alert {
+    color: #9e9e9e;
+  }
+
+</style>
