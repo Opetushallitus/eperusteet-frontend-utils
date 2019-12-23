8937ee0db9a56faed0c73c013f8fbb51107b8d34
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-12-23 09:42:07 +0200

    EP-1948

---
+++ vue/src/components/EpConfirmDialog/EpConfirmDialog.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+  <b-modal ref="epConfirmDialogModal" id="epConfirmDialog" size="lg" v-model="visible" @ok="cancelSave">
+    <template v-slot:modal-title>
+     {{ $t('haluatko-poistua-tallentamatta')}}
+    </template>
+
+    <span>{{$t('poistumisen-varmistusteksti-dialogi')}}</span>
+
+    <template v-slot:modal-cancel>
+      {{ $t('peruuta')}}
+    </template>
+    <template v-slot:modal-ok>
+      {{ $t('poistu-tallentamatta')}}
+    </template>
+
+  </b-modal>
+</div>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue, Mixins } from 'vue-property-decorator';
+import _ from 'lodash';
+import { EditointiKontrolli } from '@/stores/editointi';
+
+@Component({name:'EpConfirmDialog'})
+export default class EpConfirmDialog extends Vue {
+
+  @Prop({ required:true })
+  private redirect!: Function;
+
+  @Prop({ required:true })
+  private ctrls!: EditointiKontrolli;
+
+  private visible: boolean = false;
+
+  setVisible() {
+    this.visible = true;
+  }
+
+  cancelSave() {
+    this.ctrls.cancel();
+    this.redirect();
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
