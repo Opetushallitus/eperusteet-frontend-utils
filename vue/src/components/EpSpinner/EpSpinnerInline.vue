9c9a63b700ae8b42553ccb692d245028e6b748ce
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-01-03 10:56:05 +0200

    EP-2011

--- vue/src/components/EpSpinner/EpSpinnerInline.vue
+++ vue/src/components/EpSpinner/EpSpinnerInline.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="oph-spinner oph-spinner-in-button">
+<div class="oph-spinner oph-spinner-in-button" :class="{'link' : link}">
   <div class="oph-bounce oph-bounce1"></div>
   <div class="oph-bounce oph-bounce2"></div>
   <div class="oph-bounce oph-bounce3"></div>
@@ -7,14 +7,23 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 
 @Component
-export default class EpSpinnerInline extends Vue {}
+export default class EpSpinnerInline extends Vue {
+
+  @Prop({required: false, default: false})
+  private link!: boolean;
+
+}
 </script>
 
 <style scoped lang="scss">
 @import "./style.scss";
+
+  .oph-spinner-in-button.oph-spinner-in-button.link .oph-bounce {
+    background-color: #3367E3;
+  }
 
 .oph-spinner-in-button {
   margin-left: 15px;
