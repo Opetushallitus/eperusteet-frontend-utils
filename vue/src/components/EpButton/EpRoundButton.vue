d264d501a506bd161a5e47c18a38ea77a27f120a
Sauli Anto <sauli.anto@gmail.com>
2020-01-16 19:53:26 +0200

    Refaktoroitu komponentteja, päivitetty ops listaus näkymää ja toteutettu versiohistoriaa

--- vue/src/components/EpButton/EpRoundButton.vue
+++ vue/src/components/EpButton/EpRoundButton.vue
@@ -1,65 +1,66 @@
 <template>
-<button class="round-button"
-  :class="'round-button-' + variant"
-  v-bind="$attrs"
-  :disabled="disabled"
-  @click="$emit('click')">
-  <fas fixed-width :icon="icon"></fas>
-</button>
+  <button class="round-button"
+          :class="'round-button-' + variant"
+          v-bind="$attrs"
+          :disabled="disabled"
+          @click="$emit('click')">
+    <fas :icon="icon" fixed-width />
+  </button>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+  import { Vue, Component, Prop } from 'vue-property-decorator';
 
-import EpSpinnerInline from '../EpSpinner/EpSpinnerInline.vue';
+  import EpSpinnerInline from '@shared/components/EpSpinner/EpSpinnerInline.vue';
 
-@Component({
-  components: {
-    EpSpinnerInline,
-  },
-})
-export default class EpButton extends Vue {
-  @Prop({
-    required: true,
-    type: String,
+
+  @Component({
+    components: {
+      EpSpinnerInline,
+    },
   })
-  private icon!: string;
+  export default class EpButton extends Vue {
+    @Prop({
+      required: true,
+      type: String,
+    })
+    private icon!: string;
 
-  @Prop({
-    default: false,
-    type: Boolean,
-  })
-  private disabled!: boolean;
+    @Prop({
+      default: false,
+      type: Boolean,
+    })
+    private disabled!: boolean;
 
-  @Prop({ default: 'primary', type: Boolean })
-  private variant!: boolean;
-}
+    @Prop({ default: 'primary' })
+    private variant!: string;
+  }
 </script>
 
 <style lang="scss" scoped>
 
-.round-button {
-  border-radius: 100%;
-  background: #668eea;
-  border: none;
-  color: #fff;
-  display: inline-block;
-  min-height: 32px;
-  min-width: 32px;
-  outline: none;
-  font-size: 1.2em;
-}
+  .round-button {
+    border-radius: 100%;
+    background: #668eea;
+    border: none;
+    color: #fff;
+    display: inline-block;
+    min-height: 40px;
+    min-width: 40px;
+    outline: none;
+    font-size: 1.2em;
+  }
 
-.round-button-lightblue {
-  background: #668eea;
-}
+  .round-button-lightblue {
+    background: #3D6EC9;
+  }
 
-.round-button-green {
-  background: #f166c0;
-}
+  .round-button-green {
+    background: #D26EAF;
+  }
 
-.round-button-pink {
-  background: #62ac32;
-}
+  .round-button-pink {
+    background: #62ac32;
+  }
 
 </style>
