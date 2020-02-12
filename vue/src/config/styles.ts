ae456bf5bec866489c912ef3601531285c249b40
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-02-12 13:13:35 +0200

    styles

--- vue/src/config/styles.ts
+++ vue/src/config/styles.ts
@@ -1,4 +1,4 @@
-import '@/styles/bootstrap.scss';
+import '@shared/styles/bootstrap.scss';
 import 'katex/dist/katex.min.css';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import 'vue-multiselect/dist/vue-multiselect.min.css';
