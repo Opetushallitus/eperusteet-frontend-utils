3c50b676f6e7bd6412a96d7f1f3681daa08e6134
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-02-12 16:28:48 +0200

    styles fixes

--- vue/src/config/styles.ts
+++ vue/src/config/styles.ts
@@ -1,4 +1,5 @@
 import '@shared/styles/bootstrap.scss';
-import 'katex/dist/katex.min.css';
+import '@shared/styles/app.scss';
+// import 'katex/dist/katex.min.css';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import 'vue-multiselect/dist/vue-multiselect.min.css';
