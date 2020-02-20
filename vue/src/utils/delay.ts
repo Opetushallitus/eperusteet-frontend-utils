15668657281a8134492e65f5321b30f5f2a0e21f
nkala <niko.junkala@gofore.com>
2020-02-20 18:54:22 +0200

    Refaktorointia

--- vue/src/utils/delay.ts
+++ vue/src/utils/delay.ts
@@ -1,3 +1,10 @@
+import * as _ from 'lodash';
+import Vue from 'vue'
+import VueCompositionApi, { computed } from '@vue/composition-api';
+
+Vue.use(VueCompositionApi);
+
+
 /**
  * Asyncronic debounce that supports awaiting. Useful when need to wait
  * for the actual event to happen.
@@ -8,15 +15,17 @@
  */
 export function asyncDebounce<T, F>(fn: F & any, ms: number): F & any {
   let tid = null as number | null;
-  const wrapper = (...params: any[]) => {
+  const wrapper = function(this: any, ...params: any[]) {
     if (_.isNumber(tid)) {
       clearTimeout(tid);
     }
+    const self = this;
     return new Promise((resolve, reject) => {
       tid = setTimeout(async () => {
         tid = null;
         try {
-          resolve(await fn(...params));
+          const bound = fn.bind(self);
+          resolve(await bound(params));
         }
         catch (err) {
           reject(err);
@@ -25,6 +34,38 @@ export function asyncDebounce<T, F>(fn: F & any, ms: number): F & any {
     });
   }
   return wrapper as unknown as F;
+}
+
+
+/**
+ * Debounced that can be used for functions as a decorator
+ *
+ * @param {number} ms
+ */
+export function Debounced(ms = 300) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const original = descriptor.value;
+    descriptor.value = asyncDebounce(async function(this: any, ...params: any[]) {
+      return await original.apply(this, ...params);
+    }, ms);
+  };
+}
+
+
+/**
+ * Wraps a getter into a computed value
+ */
+export function Computed() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    if (!_.isFunction(descriptor.get)) {
+      throw new Error('descriptor must have a getter');
+    }
+    const original = descriptor.get;
+    const getter = computed(() => original);
+    descriptor.value = {
+      get: getter.value
+    };
+  };
 }
 
 
@@ -39,3 +80,5 @@ export async function delay(ms: number) {
     setTimeout(resolve, ms);
   });
 }
+
+
