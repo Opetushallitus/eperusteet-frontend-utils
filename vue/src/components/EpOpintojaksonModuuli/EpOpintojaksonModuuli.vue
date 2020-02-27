f7eb70e0725e189295a21e12b08b0caf9b081a93
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-02-27 13:45:38 +0200

    EP-2088

--- vue/src/components/EpOpintojaksonModuuli/EpOpintojaksonModuuli.vue
+++ vue/src/components/EpOpintojaksonModuuli/EpOpintojaksonModuuli.vue
@@ -14,22 +14,52 @@
     </div>
   </router-link>
 </div>
+<div v-else-if="moduuli">
+  <h2 class="otsikko" slot="header">{{ $kaanna(moduuli.nimi) + (koodi ? ' (' + koodi.arvo + ')'  : '') }}</h2>
+
+  <div class="teksti">
+  <moduuli-esitys :moduuli="moduuli"
+                  :termit="perusteTermit"
+                  :kuvat="perusteKuvat"
+                  :isPerusteView="false" />
+  </div>
+</div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import EpColorIndicator from '../EpColorIndicator/EpColorIndicator.vue';
 import { Lops2019ModuuliDto } from '../../api/tyypit';
+import { OpetussuunnitelmaDataStore } from '@/stores/OpetussuunnitelmaDataStore';
+import ModuuliEsitys from '@/routes/perusteet/sisalto/lops2019/oppiaineet/ModuuliEsitys.vue';
 
 @Component({
   components: {
     EpColorIndicator,
+    ModuuliEsitys,
   },
 })
 export default class EpOpintojaksonModuuli extends Vue {
 
   @Prop({ required: true })
+  private opetussuunnitelmaDataStore!: OpetussuunnitelmaDataStore;
+
+  @Prop({ required: true })
   private moduuli!: Lops2019ModuuliDto;
+
+  get koodi() {
+    if (this.moduuli) {
+      return this.moduuli.koodi;
+    }
+  }
+
+  get perusteTermit() {
+    return this.opetussuunnitelmaDataStore.perusteTermit;
+  }
+
+  get perusteKuvat() {
+    return this.opetussuunnitelmaDataStore.perusteKuvat;
+  }
 }
 </script>
 
