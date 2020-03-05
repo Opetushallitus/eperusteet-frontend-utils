dfa04d4f9ef7a6e2732b0548d3da60461b156afe
nkala <niko.junkala@gofore.com>
2020-03-05 19:21:32 +0200

    Lisätty tyypitykset

--- vue/src/plugins/kaannos.ts
+++ vue/src/plugins/kaannos.ts
@@ -4,6 +4,13 @@ import { Kielet } from '../stores/kieli';
 
 const logger = createLogger('Kaannos');
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    $kaanna: <T extends object>(value: T) => string;
+    $kaannaOlioTaiTeksti: <T extends object>(value: T | string) => string;
+  }
+}
+
 export class Kaannos {
   public install(vue: typeof Vue) {
     // Sisällön kääntäminen
