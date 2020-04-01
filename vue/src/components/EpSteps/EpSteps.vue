74cbcb0d16baece6bbe7836f4979d7ba952bf281
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-04-01 17:34:19 +0300

    EP-2143

--- vue/src/components/EpSteps/EpSteps.vue
+++ vue/src/components/EpSteps/EpSteps.vue
@@ -65,6 +65,9 @@ export default class EpSteps extends Vue {
   @Prop({ required: true })
   private onSave!: () => Promise<void>;
 
+  @Prop({ default: true })
+  private valid!: boolean;
+
   private stepIdx = 0;
 
   async saveImpl() {
@@ -78,6 +81,13 @@ export default class EpSteps extends Vue {
   @Watch('initialStep', { immediate: true })
   onInitialStepUpdate(value: number) {
     this.stepIdx = value;
+  }
+
+  @Watch('stepIdx', { immediate: true })
+  onStepChange(val) {
+    if (this.$listeners && this.$listeners.stepChange) {
+      this.$emit('stepChange', this.currentStep.key);
+    }
   }
 
   get currentStep() {
