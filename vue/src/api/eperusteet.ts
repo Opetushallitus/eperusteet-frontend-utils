f6cc3a6c7bb8523e74480d36244c459992e8f7a0
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-04-08 15:08:24 +0300

    EP-2137

--- vue/src/api/eperusteet.ts
+++ vue/src/api/eperusteet.ts
@@ -59,6 +59,7 @@ export const TutkinnonRakenne = initApi(EperusteetApi.TutkinnonRakenneApi);
 export const Tutkinnonosat = initApi(EperusteetApi.TutkinnonosatApi);
 export const TutkinnonosatPrivate = initApi(EperusteetApi.TutkinnonosatPrivateApi);
 export const Ulkopuoliset = initApi(EperusteetApi.UlkopuolisetApi);
+export const Maintenance = initApi(EperusteetApi.MaintenanceApi);
 
 export type Ammattitaitovaatimukset2019Dto = EperusteetApi.Ammattitaitovaatimukset2019Dto;
 export type ArviointiAsteikkoDto = EperusteetApi.ArviointiAsteikkoDto;
