1792f042b823aa362e09705992e3b39324cba256
Sauli Anto <sauli.anto@gmail.com>
2020-04-09 10:43:40 +0300

    Näytetään kaikki virheet heti

--- vue/scripts/check-versions.js
+++ vue/scripts/check-versions.js
@@ -3,13 +3,17 @@ const fs = require('fs');
 function main() {
     const package = JSON.parse(fs.readFileSync('package.json').toString('utf8'));
     const utilsPackage = JSON.parse(fs.readFileSync('eperusteet-frontend-utils/vue/package.json').toString('utf8'));
-
+    let error = false;
     for (const [name, version] of Object.entries(package.dependencies)) {
         const utilPackageVersion = utilsPackage.dependencies[name];
         if (utilPackageVersion && utilPackageVersion !== version) {
-            console.error(`\nFix ${name} version mismatch (${version} -> ${utilPackageVersion})\n`);
-            process.exit(1);
+            console.error(`Fix ${name} version mismatch (${version} -> ${utilPackageVersion})`);
+            error = true;
         }
+    }
+
+    if (error) {
+        process.exit(1);
     }
 }
 
