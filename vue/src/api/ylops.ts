fe783862630974cc1214b91a1d7cf82eb2ce14db
Sauli Anto <sauli.anto@gmail.com>
2020-06-18 16:17:40 +0300

    Päivitetty rajapinta

--- vue/src/api/ylops.ts
+++ vue/src/api/ylops.ts
@@ -123,6 +123,7 @@ export type OpetussuunnitelmanAikatauluDto = YlopsApi.OpetussuunnitelmanAikataul
 export type PerusteInfoDto = YlopsApi.PerusteInfoDto;
 export type PerusteTekstiKappaleViiteDto = YlopsApi.PerusteTekstiKappaleViiteDto;
 export type PerusteTekstiKappaleViiteMatalaDto = YlopsApi.PerusteTekstiKappaleViiteMatalaDto;
+export type TekstiKappaleViiteDto = YlopsApi.TekstiKappaleViiteDto;
 export type PoistettuTekstiKappaleDto = YlopsApi.PoistettuTekstiKappaleDto;
 export type Puu = YlopsApi.Puu;
 export type RevisionDto = YlopsApi.RevisionDto;
