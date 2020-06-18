1b77ab28728189f9c1d21c1df9c693efbe5d12b0
Sauli Anto <sauli.anto@gmail.com>
2020-06-18 13:38:32 +0300

    Korjattu testit

--- vue/src/components/EpContent/EpContent.spec.ts
+++ vue/src/components/EpContent/EpContent.spec.ts
@@ -1,9 +1,12 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+
 import EpEditorMenuBar from './EpEditorMenuBar.vue';
 import EpContent from './EpContent.vue';
 import { Kielet } from '../../stores/kieli';
 import { Kieli } from '../../tyypit';
+import { Kaannos } from '../../plugins/kaannos';
 import { Editor } from 'tiptap';
 import '@/config/bootstrap';
 import '@/config/fontawesome';
@@ -78,7 +81,9 @@ describe('EpContent component', () => {
   });
 
   const localVue = createLocalVue();
+  localVue.use(VueI18n);
   Kielet.install(localVue);
+  localVue.use(new Kaannos());
   const propsData = {
     help: '',
     layout: 'simplified',
@@ -129,13 +134,13 @@ describe('EpContent component', () => {
     expect(wrapper.html()).not.toContain('teksti1234');
     expect(wrapper.html()).toContain('sv');
   });
-
-  return wrapper;
 });
 
 describe('EpContentMenu component', () => {
   const localVue = createLocalVue();
+  localVue.use(VueI18n);
   Kielet.install(localVue);
+  localVue.use(new Kaannos());
 
   it('Hide menu when read only', async () => {
     const wrapper = createWrapper(localVue, {
