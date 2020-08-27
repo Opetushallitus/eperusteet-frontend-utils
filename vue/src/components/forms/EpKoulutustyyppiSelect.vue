bb355b631c8db0b58d2491973f0b9c02ff3599be
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-08-27 15:13:55 +0300

    EP-2359

--- vue/src/components/forms/EpKoulutustyyppiSelect.vue
+++ vue/src/components/forms/EpKoulutustyyppiSelect.vue
@@ -1,19 +1,30 @@
 <template>
-  <EpMultiSelect v-model="value"
-                 :placeholder="$t('valitse-koulutustyyppi')"
+  <EpMultiSelect :value="value"
+                 @input="changed($event)"
+                 :placeholder="$t('kaikki')"
                  :search-identity="identity"
                  v-if="isEditing"
-                 :options="vaihtoehdot">
+                 :options="kaikkiVaihtoehdot"
+                 group-values="koulutustyypit"
+                 group-label="ryhma"
+                 :group-select="false"
+                 :searchable="false"
+                 :maxHeight="500">
+
   <template slot="singleLabel" slot-scope="{ option }">
     <span class="text-nowrap">
-      <EpColorIndicator :size="10" :kind="option" />
+      <EpColorIndicator :size="10" :kind="option"/>
       <span class="ml-2">{{ $t(option) }}</span>
     </span>
   </template>
   <template slot="option" slot-scope="{ option }">
-    <span class="text-nowrap">
-      <EpColorIndicator :size="10" :kind="option" />
-      <span class="ml-2">{{ $t(option) }}</span>
+    <hr class="mt-0 mb-0" v-if="option.$groupLabel" />
+
+    <span v-else class="text-nowrap">
+      <EpColorIndicator :size="10" :kind="option" v-if="option !== 'kaikki'"/>
+      <span :class="{'font-weight-bold': option === 'kaikki', 'ml-2': option !== 'kaikki'}">
+        {{ $t(option) }}
+      </span>
     </span>
   </template>
   </EpMultiSelect>
@@ -38,7 +49,7 @@ import EpButton from '@shared/components/EpButton/EpButton.vue';
 import EpSteps from '@shared/components/EpSteps/EpSteps.vue';
 import EpAikataulu from '@shared/components/EpAikataulu/EpAikataulu.vue';
 import EpColorIndicator from '@shared/components/EpColorIndicator/EpColorIndicator.vue';
-import { EperusteetKoulutustyypit } from '../../utils/perusteet';
+import { EperusteetKoulutustyypit, EperusteetKoulutustyyppiRyhmat } from '../../utils/perusteet';
 import { BvTableFieldArray } from 'bootstrap-vue';
 import * as _ from 'lodash';
 
@@ -66,8 +77,33 @@ export default class KoulutustyyppiSelect extends Vue {
     return _.toLower(this.$kaanna(tr.nimi));
   }
 
+  private changed(value: any) {
+    if (value === 'kaikki') {
+      value = undefined;
+    }
+    this.$emit('input', value);
+  }
+
+  get kaikkiVaihtoehdot() {
+    return [
+      {
+        ryhma: 'kaikki',
+        koulutustyypit: ['kaikki'],
+      },
+      ...this.vaihtoehdot,
+    ];
+  }
+
   get vaihtoehdot() {
-    return _.sortBy(this.koulutustyypit, kt => this.$t(kt));
+    return _.chain(EperusteetKoulutustyyppiRyhmat)
+      .keys()
+      .map(ryhma => {
+        return {
+          ryhma,
+          koulutustyypit: _.filter(EperusteetKoulutustyyppiRyhmat[ryhma], koulutustyyppi => _.isEmpty(this.koulutustyypit) || _.includes(this.koulutustyypit, koulutustyyppi)),
+        };
+      })
+      .value();
   }
 }
 </script>
@@ -80,6 +116,11 @@ export default class KoulutustyyppiSelect extends Vue {
   .nimi {
     font-weight: 600;
   }
+}
+
+::v-deep .multiselect__option--disabled {
+  padding: 0;
+  min-height: 0;
 }
 
 </style>
