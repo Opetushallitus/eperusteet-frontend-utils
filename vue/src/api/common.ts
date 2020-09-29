b3a8c432f2866f570e150c1bbc11197bee2adf76
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-09-29 15:14:48 +0300

    EP-2293-fix2

--- vue/src/api/common.ts
+++ vue/src/api/common.ts
@@ -41,7 +41,7 @@ export function successfulResponseHandler() {
 
 export async function getCasKayttajaKieli() {
   try {
-    const casKayttaja = await axios.get(`${location.protocol}//${location.host}/cas/me`);
+    const casKayttaja = _.get(await axios.get(`${location.protocol}//${location.host}/cas/me`), 'data');
     return _.get(casKayttaja, 'lang');
   }
   catch (e) {
