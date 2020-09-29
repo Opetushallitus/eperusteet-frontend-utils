d9cd4a5867fafd4d09e428c85c0301fd326da8f7
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-09-29 09:59:20 +0300

    EP-2413

---
+++ vue/src/components/EpArvioinninkohteetTable/EpArvioinninkohteetTable.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-table :items="arvioinninkohteet" :fields="sarakkeet"/>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import * as _ from 'lodash';
+
+@Component
+export default class EpArvioinninkohteetTable extends Vue {
+  @Prop({ required: true })
+  private arvioinninkohteet!: any[];
+
+  get osaamisenKuvauksetTyhjia() {
+    return _.size(_.filter(this.arvioinninkohteet, kohde => kohde.arvosana !== null)) === 0;
+  }
+
+  get sarakkeet() {
+    if (this.osaamisenKuvauksetTyhjia) {
+      return [
+        this.osaamisenKuvausSarake,
+      ];
+    }
+
+    return [
+      this.arvosanaSarake,
+      this.osaamisenKuvausSarake,
+    ];
+  }
+
+  get arvosanaSarake() {
+    return {
+      key: 'arvosana',
+      label: this.$t('osaamistaso'),
+      thStyle: { width: '20%' },
+      formatter: (value, key, item) => {
+        if (value) {
+          return (this as any).$t('arvosana') + ' ' + value;
+        }
+
+        return '';
+      },
+    };
+  }
+
+  get osaamisenKuvausSarake() {
+    return {
+      key: 'osaamisenKuvaus',
+      label: this.$t('kuvaus'),
+      formatter: (value, key, item) => {
+        return (this as any).$kaanna(value);
+      },
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
