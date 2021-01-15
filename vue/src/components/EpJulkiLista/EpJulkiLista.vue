e1982896f331cb1c51b8e902046532be38d0f51d
Mikael Pursiainen <15607612+pursi@users.noreply.github.com>
2021-01-15 15:55:10 +0200

    EP-2562 (#330)
    * EP-2562
    
    * Revert EpJulkiLista

--- vue/src/components/EpJulkiLista/EpJulkiLista.vue
+++ vue/src/components/EpJulkiLista/EpJulkiLista.vue
@@ -36,18 +36,15 @@
 </template>
 
 <script lang="ts">
-
 import { Vue, Component, Prop, Mixins, Watch } from 'vue-property-decorator';
 import _ from 'lodash';
 import EpSpinner from '../EpSpinner/EpSpinner.vue';
 import EpButton from '../EpButton/EpButton.vue';
-
 export interface JulkiRivi {
   otsikko?: { [key: string]: string; } | string;
   uusi: boolean;
   muokattu?: Date;
 }
-
 @Component({
   components: {
     EpSpinner,
@@ -57,30 +54,23 @@ export interface JulkiRivi {
 export default class EpJulkiLista extends Vue {
   @Prop({ required: true })
   private tiedot!: JulkiRivi[];
-
   @Prop({ required: false, default: null })
   private tietoMaara!: number;
-
   @Prop({ required: false, default: 'lisahaku' })
   private listausTyyppi!: 'sivutus' | 'lisahaku';
-
   private naytettavaTietoMaara = 3;
   private sivu = 1;
-
   mounted() {
     if (this.tietoMaara) {
       this.naytettavaTietoMaara = this.tietoMaara;
     }
   }
-
   get hasClickEvent() {
     return this.$listeners && this.$listeners.avaaTieto;
   }
-
   get tiedotSize() {
     return _.size(this.tiedot);
   }
-
   get tiedotFiltered() {
     if (this.tiedot) {
       return _.chain(this.tiedot)
@@ -95,15 +85,12 @@ export default class EpJulkiLista extends Vue {
         .value();
     }
   }
-
   avaaTieto(tieto: JulkiRivi) {
     this.$emit('avaaTieto', tieto);
   }
-
   tuntisitten(aika) {
     const tunti = 1000 * 60 * 60;
     const tuntisitten = Date.now() - tunti;
-
     return aika > tuntisitten;
   }
 }
@@ -111,30 +98,22 @@ export default class EpJulkiLista extends Vue {
 
 <style scoped lang="scss">
 @import "@shared/styles/_variables.scss";
-
   .content {
-
     .tieto:nth-of-type(even) {
       background-color: $table-even-row-bg-color;
     }
-
     .tieto:nth-of-type(odd) {
       background-color: $table-odd-row-bg-color;
     }
-
     .tieto {
-
       &.clickable:hover{
         background-color: $table-hover-row-bg-color;
         cursor: pointer;
       }
-
       .otsikko {
-
         &.uusi {
           font-weight: bold;
         }
-
         .uusi {
           background-color: $blue-lighten-3;
           border-radius: 5px;
@@ -143,16 +122,12 @@ export default class EpJulkiLista extends Vue {
           margin-left: 5px;
         }
       }
-
       .muokkausaika {
         color: $gray-lighten-1;
       }
     }
-
     ::v-deep .btn {
       padding: 0px;
     }
-
   }
-
 </style>
