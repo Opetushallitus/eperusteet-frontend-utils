68282a15c5fbf95e6c487de0e1c1f74ced5137fa
Mikael Pursiainen <mikael.pursiainen@gofore.com>
2021-02-09 11:26:19 +0200

    EP-2589 refactor EpTiedoteView

--- vue/src/components/EpTiedoteView/EpTiedoteView.vue
+++ vue/src/components/EpTiedoteView/EpTiedoteView.vue
@@ -1,5 +1,5 @@
 <template>
-  <ep-main-view :container="true" class="mt-5">
+  <ep-main-view :container="true" class="mt-5" v-if="tiedotteet">
     <template slot="header">
       <div class="d-flex justify-content-between align-items-center">
         <h1>{{ $t('tiedotteet') }}</h1>
@@ -18,7 +18,7 @@
 
     <div class="row align-items-end mb-4">
       <div class="col-4">
-        <ep-search v-model="nimiFilter" @input="nimiFilterChanged" :is-loading="isLoading" />
+        <slot name="search"></slot>
       </div>
     </div>
 
@@ -34,12 +34,7 @@
           <h2 class="font-weight-normal">{{ $kaanna(tiedote.otsikko) }}</h2>
         </template>
       </ep-content-read-more>
-      <b-pagination
-        v-model="currentPage"
-        :total-rows="totalRows"
-        :per-page="perPage"
-        @change="pageChanged"
-        align="center" />
+      <slot name="pagination"></slot>
     </template>
 
   </ep-main-view>
@@ -47,84 +42,31 @@
 
 <script lang="ts">
 import _ from 'lodash';
-import { Prop, Vue, Component, Watch } from 'vue-property-decorator';
+import { Prop, Vue, Component } from 'vue-property-decorator';
 
 import EpMainView from '../EpMainView/EpMainView.vue';
 import EpSearch from '../forms/EpSearch.vue';
-import EpTiedoteModal from '../EpTiedoteModal/EpTiedoteModal.vue';
-import EpSpinner from '../EpSpinner/EpSpinner.vue';
-import EpButton from '../EpButton/EpButton.vue';
 import EpContentReadMore from '../EpContentReadMore/EpContentReadMore.vue';
 import EpLinkki from '../EpLinkki/EpLinkki.vue';
 
 import { Kielet } from '../../stores/kieli';
-import { TiedoteViewStore } from './TiedoteViewStore';
+
+import { TiedoteDto } from '../../tyypit';
 
 @Component({
   components: {
     EpMainView,
     EpSearch,
-    EpTiedoteModal,
-    EpSpinner,
-    EpButton,
     EpContentReadMore,
     EpLinkki,
   },
 })
 export default class EpTiedoteView extends Vue {
-  private nimiFilter = '';
-  private currentPage = 1;
-  private tiedoteViewStore = new TiedoteViewStore();
-
-  @Prop({ required: false })
-  private tiedoteJulkaisuPaikka!: string;
-
-  @Watch('sisaltoKieli', { immediate: true })
-  async onSisaltoKieliChange(newValue: string, oldValue: string) {
-    if (newValue && newValue !== oldValue) {
-      this.currentPage = 1;
-      this.tiedoteViewStore.changeLang(newValue);
-    }
-  }
-
-  async mounted() {
-    this.tiedoteViewStore.init({
-      sivu: this.currentPage - 1,
-      sivukoko: 10,
-      ...this.tiedoteJulkaisuPaikka && {
-        tiedoteJulkaisuPaikka: [this.tiedoteJulkaisuPaikka],
-      },
-    });
-  }
-
-  nimiFilterChanged(value) {
-    this.nimiFilter = value;
-    this.tiedoteViewStore.changeNimiFilter(this.nimiFilter);
-  }
-
-  pageChanged(value) {
-    this.currentPage = value;
-    this.tiedoteViewStore.changePage(this.currentPage - 1);
-  }
+  @Prop({ required: true })
+  private tiedotteet!: TiedoteDto;
 
   get sisaltoKieli() {
     return Kielet.getSisaltoKieli.value || null;
-  }
-
-  get perPage() {
-    return this.tiedoteViewStore.options.value?.sivukoko;
-  }
-
-  get totalRows() {
-    return this.tiedoteViewStore.kokonaismaara.value;
-  }
-
-  get tiedotteet() {
-    return this.tiedoteViewStore.tiedotteet.value;
-  }
-
-  get isLoading() {
-    return this.tiedoteViewStore.isLoading.value;
   }
 
   get url() {
