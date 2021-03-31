46effd79941d92a21202dac41c0305e0c4462bed
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-03-31 14:29:09 +0300

    EP-2703: opintopolku

--- vue/src/components/EpTavoitesisaltoalue/EpTavoitesisaltoalueTavoitealueet.vue
+++ vue/src/components/EpTavoitesisaltoalue/EpTavoitesisaltoalueTavoitealueet.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
-    <b-row v-for="(tavoitealue, tavoitealueIndex) in tavoitealueet" :key="'tavoite'+tavoitealueIndex" class="tavoitealue">
-      <b-col cols="8">
-        <template v-if="tavoitealue.tavoiteAlueTyyppi === 'OTSIKKO'">
-          <hr v-if="tavoitealueIndex > 0" class="mt-0"/>
-          <div class="otsikko mb-2">{{$kaanna(tavoitealue.otsikko.nimi)}}</div>
-        </template>
+    <div v-for="(tavoitealue, tavoitealueIndex) in tavoitealueet" :key="'tavoite'+tavoitealueIndex" class="tavoitealue">
+      <template v-if="tavoitealue.tavoiteAlueTyyppi === 'OTSIKKO'">
+        <hr v-if="tavoitealueIndex > 0" class="mt-0"/>
+        <div class="otsikko mb-2">{{$kaanna(tavoitealue.otsikko.nimi)}}</div>
+      </template>
 
-        <div v-if="tavoitealue.tavoiteAlueTyyppi === 'TAVOITESISALTOALUE'" class="tavoitesisaltoalue mb-4">
+      <div v-if="tavoitealue.tavoiteAlueTyyppi === 'TAVOITESISALTOALUE'" class="tavoitesisaltoalue mb-4">
 
-          <div v-if="tavoitealue.tavoitteet && tavoitealue.tavoitteet.length > 0" class="mb-4">
-            <div class="tavoitteet mb-2">{{$t('tavoitteet')}}</div>
-            <div v-for="(tavoite, index) in tavoitealue.tavoitteet" :key="tavoitealueIndex+'tavoitealue'+index" class="tavoite mb-2">
-              {{$kaanna(tavoite.nimi)}}
-            </div>
+        <div v-if="tavoitealue.tavoitteet && tavoitealue.tavoitteet.length > 0" class="mb-4">
+          <div class="tavoitteet mb-2">{{$t('tavoitteet')}}</div>
+          <div v-for="(tavoite, index) in tavoitealue.tavoitteet" :key="tavoitealueIndex+'tavoitealue'+index" class="tavoite mb-2">
+            {{$kaanna(tavoite.nimi)}}
           </div>
-
-          <div v-if="tavoitealue.keskeisetSisaltoalueet && tavoitealue.keskeisetSisaltoalueet.length > 0">
-            <div class="tavoitteet mb-2">{{$t('keskeiset-sisaltoalueet')}}</div>
-            <div v-for="(keskeisetSisaltoalueet, index) in tavoitealue.keskeisetSisaltoalueet" :key="tavoitealueIndex+'tavoitealue'+index" class="keskeinensisaltoalue">
-              {{$kaanna(keskeisetSisaltoalueet)}}
-            </div>
-          </div>
-
         </div>
-      </b-col>
-    </b-row>
+
+        <div v-if="tavoitealue.keskeisetSisaltoalueet && tavoitealue.keskeisetSisaltoalueet.length > 0">
+          <div class="tavoitteet mb-2">{{$t('keskeiset-sisaltoalueet')}}</div>
+          <div v-for="(keskeisetSisaltoalueet, index) in tavoitealue.keskeisetSisaltoalueet" :key="tavoitealueIndex+'tavoitealue'+index" class="keskeinensisaltoalue">
+            {{$kaanna(keskeisetSisaltoalueet)}}
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
