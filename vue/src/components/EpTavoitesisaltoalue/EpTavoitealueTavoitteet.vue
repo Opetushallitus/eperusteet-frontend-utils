a2bd5da437708577df11ccda7374d2363204520f
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-04-01 15:16:46 +0300

    EP-2703: eperusteet

---
+++ vue/src/components/EpTavoitesisaltoalue/EpTavoitealueTavoitteet.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <draggable
+      v-bind="tavoitteetOptions"
+      tag="div"
+      v-model="tavoitteet">
+      <b-row v-for="(tavoite, tavoiteIndex) in tavoitteet" :key="tavoite+tavoiteIndex" class="pb-2">
+        <b-col cols="11">
+          <EpKoodistoSelect
+            :store="tavoitteetlukutaidotKoodisto"
+            v-model="tavoitteet[tavoiteIndex]"
+            :is-editing="true"
+            :naytaArvo="false">
+            <template #default="{ open }">
+              <b-input-group>
+                <EpInput
+                  v-model="tavoite.nimi"
+                  :is-editing="true"
+                  :disabled="!!tavoite.uri"
+                  class="input-wrapper">
+                  <div class="order-handle m-2" slot="left">
+                    <fas icon="grip-vertical"></fas>
+                  </div>
+                </EpInput>
+                <b-input-group-append>
+                  <b-button @click="open" icon="plus" variant="primary">
+                    {{ $t('hae-koodistosta') }}
+                  </b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </template>
+          </EpKoodistoSelect>
+        </b-col>
+        <b-col cols="1">
+          <fas icon="roskalaatikko" class="default-icon clickable mt-2" @click="poistaTavoite(tavoite)"/>
+        </b-col>
+      </b-row>
+    </draggable>
+
+    <ep-button variant="outline" icon="plus" @click="lisaaTavoite()">
+      {{ $t('lisaa-tavoite') }}
+    </ep-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Watch, Component, Prop, Vue } from 'vue-property-decorator';
+import _ from 'lodash';
+import { KoodistoSelectStore } from '../EpKoodistoSelect/KoodistoSelectStore';
+import { Koodisto } from '@shared/api/eperusteet';
+import draggable from 'vuedraggable';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
+import EpKoodistoSelect from '@shared/components/EpKoodistoSelect/EpKoodistoSelect.vue';
+import EpInput from '@shared/components/forms/EpInput.vue';
+
+@Component({
+  components: {
+    EpKoodistoSelect,
+    EpButton,
+    draggable,
+    EpInput,
+  },
+})
+export default class EpTavoitealueTavoitteet extends Vue {
+  @Prop({ required: true })
+  private value!: any[];
+
+  get tavoitteet() {
+    return this.value;
+  }
+
+  set tavoitteet(value) {
+    this.$emit('input', value);
+  }
+
+  private readonly tavoitteetlukutaidotKoodisto = new KoodistoSelectStore({
+    async query(query: string, sivu = 0) {
+      const { data } = (await Koodisto.kaikkiSivutettuna('tavoitteetlukutaidot', query, {
+        params: {
+          sivu,
+          sivukoko: 10,
+        },
+      }));
+      return data as any;
+    },
+  });
+
+  lisaaTavoite() {
+    this.tavoitteet = [
+      ...this.tavoitteet,
+      {},
+    ];
+  }
+
+  poistaTavoite(tavoite) {
+    this.tavoitteet = _.filter(this.tavoitteet, rivi => rivi !== tavoite);
+  }
+
+  get defaultDragOptions() {
+    return {
+      animation: 300,
+      emptyInsertThreshold: 10,
+      handle: '.order-handle',
+      ghostClass: 'dragged',
+    };
+  }
+
+  get tavoitteetOptions() {
+    return {
+      ...this.defaultDragOptions,
+      group: {
+        name: 'tavoitteet',
+      },
+    };
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../styles/_variables.scss";
+
+</style>
--- vue/src/components/EpTavoitesisaltoalue/EpTavoitesisaltoalueTavoitealueet.vue
+++ vue/src/components/EpTavoitesisaltoalue/EpTavoitesisaltoalueTavoitealueet.vue
@@ -1,45 +1,234 @@
 <template>
   <div>
-    <div v-for="(tavoitealue, tavoitealueIndex) in tavoitealueet" :key="'tavoite'+tavoitealueIndex" class="tavoitealue">
-      <template v-if="tavoitealue.tavoiteAlueTyyppi === 'OTSIKKO'">
-        <hr v-if="tavoitealueIndex > 0" class="mt-0 mb-5"/>
-        <div class="otsikko mb-3">{{$kaanna(tavoitealue.otsikko.nimi)}}</div>
-      </template>
+    <div v-if="isEditing">
 
-      <div v-if="tavoitealue.tavoiteAlueTyyppi === 'TAVOITESISALTOALUE'" class="tavoitesisaltoalue mb-5">
+      <draggable
+        v-bind="tavoitealueOptions"
+        tag="div"
+        v-model="tavoitealueet">
 
-        <div v-if="tavoitealue.tavoitteet && tavoitealue.tavoitteet.length > 0" class="mb-5">
-          <div class="tavoitteet mb-2">{{$t('tavoitteet')}}</div>
-          <div v-for="(tavoite, index) in tavoitealue.tavoitteet" :key="tavoitealueIndex+'tavoitealue'+index" class="tavoite mb-3">
-            {{$kaanna(tavoite.nimi)}}
+        <div v-for="(tavoitealue, tavoitealueIndex) in tavoitealueet" :key="'tavoite'+tavoitealueIndex" class="pb-2 tavoitealue editing">
+          <div v-if="tavoitealue.tavoiteAlueTyyppi === 'OTSIKKO'" class="m-2">
+            <div class="order-handle m-2" slot="left">
+              <fas icon="grip-vertical"></fas>
+              <span class="otsikko"> {{$t('tavoitealueen-otsikko')}}</span>
+            </div>
+            <b-row>
+              <b-col cols="11">
+                <ep-koodisto-select :store="tavoitealueetKoodisto" v-model="tavoitealue.otsikko" :is-editing="isEditing" :naytaArvo="false">
+                  <template #default="{ open }">
+                    <b-input-group>
+                      <b-form-input
+                        :value="tavoitealue.otsikko ? $kaanna(tavoitealue.otsikko.nimi) : ''"
+                        disabled></b-form-input>
+                      <b-input-group-append>
+                        <b-button @click="open" icon="plus" variant="primary">
+                          {{ $t('hae-koodistosta') }}
+                        </b-button>
+                      </b-input-group-append>
+                    </b-input-group>
+                  </template>
+                </ep-koodisto-select>
+              </b-col>
+              <b-col cols="1">
+                <fas icon="roskalaatikko" class="default-icon clickable mt-2" @click="poistaTavoitealue(tavoitealue)"/>
+              </b-col>
+            </b-row>
+          </div>
+
+          <div v-if="tavoitealue.tavoiteAlueTyyppi === 'TAVOITESISALTOALUE'" class="m-2 tavoitesisaltoalue">
+            <div class="order-handle m-2" slot="left">
+              <fas icon="grip-vertical"></fas>
+              <span class="otsikko pl-1"> {{$t('tavoitteet')}}</span>
+            </div>
+            <div class="ml-4">
+              <EpTavoitealueTavoitteet v-model="tavoitealue.tavoitteet" />
+            </div>
+            <div class="mt-3 ml-4">
+              <div class="otsikko mb-2"> {{$t('keskeiset-sisaltoalueet')}}</div>
+              <EpTavoitealueKeskeisetSisaltoalueet v-model="tavoitealue.keskeisetSisaltoalueet"  />
+            </div>
+            <div class="text-right">
+              <ep-button variant="link" icon="roskalaatikko" @click="poistaTavoitealue(tavoitealue)">
+                  {{ $t('poista-tavoitteet-ja-sisaltoalueet') }}
+                </ep-button>
+            </div>
           </div>
         </div>
+      </draggable>
 
-        <div v-if="tavoitealue.keskeisetSisaltoalueet && tavoitealue.keskeisetSisaltoalueet.length > 0">
-          <div class="tavoitteet mb-2">{{$t('keskeiset-sisaltoalueet')}}</div>
-          <div v-for="(keskeisetSisaltoalueet, index) in tavoitealue.keskeisetSisaltoalueet" :key="tavoitealueIndex+'tavoitealue'+index" class="keskeinensisaltoalue p-2">
-            {{$kaanna(keskeisetSisaltoalueet)}}
-          </div>
-        </div>
-
+      <div class="d-flex flex-column">
+        <ep-button variant="outline" icon="plus" @click="lisaaTavoitealue('OTSIKKO')">
+          {{ $t('lisaa-tavoitealueen-otsikko') }}
+        </ep-button>
+        <ep-button variant="outline" icon="plus" @click="lisaaTavoitealue('TAVOITESISALTOALUE')">
+          {{ $t('lisaa-tavoitteet-ja-sisaltoalueet') }}
+        </ep-button>
       </div>
+
     </div>
+
+    <div v-if="!isEditing">
+
+      <div v-for="(tavoitealue, tavoitealueIndex) in tavoitealueet" :key="'tavoite'+tavoitealueIndex" class="tavoitealue">
+        <template v-if="tavoitealue.tavoiteAlueTyyppi === 'OTSIKKO'">
+          <hr v-if="tavoitealueIndex > 0" class="mt-0 mb-5"/>
+          <div class="otsikko mb-3">{{$kaanna(tavoitealue.otsikko.nimi)}}</div>
+        </template>
+
+        <div v-if="tavoitealue.tavoiteAlueTyyppi === 'TAVOITESISALTOALUE'" class="tavoitesisaltoalue mb-5">
+
+          <div v-if="tavoitealue.tavoitteet && tavoitealue.tavoitteet.length > 0" class="mb-45">
+            <div class="tavoitteet mb-2">{{$t('tavoitteet')}}</div>
+            <div v-for="(tavoite, index) in tavoitealue.tavoitteet" :key="tavoitealueIndex+'tavoitealue'+index" class="tavoite mb-3">
+              {{$kaanna(tavoite.nimi)}}
+            </div>
+          </div>
+
+          <div v-if="tavoitealue.keskeisetSisaltoalueet && tavoitealue.keskeisetSisaltoalueet.length > 0">
+            <div class="tavoitteet mb-2">{{$t('keskeiset-sisaltoalueet')}}</div>
+            <div v-for="(keskeisetSisaltoalueet, index) in tavoitealue.keskeisetSisaltoalueet" :key="tavoitealueIndex+'tavoitealue'+index" class="keskeinensisaltoalue p-2">
+              {{$kaanna(keskeisetSisaltoalueet)}}
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Watch, Component, Prop, Vue } from 'vue-property-decorator';
 import _ from 'lodash';
+import { KoodistoSelectStore } from '../EpKoodistoSelect/KoodistoSelectStore';
+import { Koodisto } from '@shared/api/eperusteet';
+import draggable from 'vuedraggable';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
+import EpKoodistoSelect from '@shared/components/EpKoodistoSelect/EpKoodistoSelect.vue';
+import EpTavoitealueKeskeisetSisaltoalueet from './EpTavoitealueKeskeisetSisaltoalueet.vue';
+import EpTavoitealueTavoitteet from './EpTavoitealueTavoitteet.vue';
 
-@Component
+@Component({
+  components: {
+    EpKoodistoSelect,
+    EpButton,
+    draggable,
+    EpTavoitealueKeskeisetSisaltoalueet,
+    EpTavoitealueTavoitteet,
+  },
+})
 export default class EpTavoitesisaltoalueTavoitealueet extends Vue {
   @Prop({ required: true })
-  private tavoitealueet!: any[];
+  private value!: any[];
+
+  @Prop({ required: false, default: false, type: Boolean })
+  private isEditing!: boolean;
+
+  get tavoitealueet() {
+    return this.value;
+  }
+
+  set tavoitealueet(value) {
+    this.$emit('input', value);
+  }
+
+  private readonly tavoitealueetKoodisto = new KoodistoSelectStore({
+    async query(query: string, sivu = 0) {
+      const { data } = (await Koodisto.kaikkiSivutettuna('tavoitealueet', query, {
+        params: {
+          sivu,
+          sivukoko: 10,
+        },
+      }));
+      return data as any;
+    },
+  });
+
+  lisaaTavoitealue(tyyppi: 'OTSIKKO' | 'TAVOITESISALTOALUE') {
+    this.tavoitealueet = [
+      ...this.tavoitealueet,
+      {
+        tavoiteAlueTyyppi: tyyppi,
+        ...(tyyppi === 'TAVOITESISALTOALUE' && { tavoitteet: [] }),
+        ...(tyyppi === 'TAVOITESISALTOALUE' && { keskeisetSisaltoalueet: [] }),
+      },
+    ];
+  }
+
+  poistaTavoitealue(tavoitealue) {
+    this.tavoitealueet = _.filter(this.tavoitealueet, rivi => rivi !== tavoitealue);
+  }
+
+  get defaultDragOptions() {
+    return {
+      animation: 300,
+      emptyInsertThreshold: 10,
+      handle: '.order-handle',
+      disabled: !this.isEditing,
+      ghostClass: 'dragged',
+    };
+  }
+  get tavoitealueOptions() {
+    return {
+      ...this.defaultDragOptions,
+      group: {
+        name: 'tavoitealue',
+      },
+    };
+  }
+
+  get tavoitteetOptions() {
+    return {
+      ...this.defaultDragOptions,
+      group: {
+        name: 'tavoitteet',
+      },
+    };
+  }
+
+  get keskeisetSisaltoalueetOptions() {
+    return {
+      ...this.defaultDragOptions,
+      group: {
+        name: 'keskeisetsisaltoalueet',
+      },
+    };
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import "../../styles/_variables.scss";
+
+  .tavoitealue {
+    &.editing {
+      .tavoitesisaltoalue {
+        border: 1px solid $gray-lighten-8;
+        border-radius: 3px;
+
+        .otsikko {
+          color: $black;
+        }
+      }
+    }
+  }
+
+  ::v-deep fieldset {
+    padding-right: 0px;
+  }
+
+  ::v-deep .input-wrapper {
+    flex: 1 1 0%;
+
+    input {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+  }
+
   .tavoitealue{
     .otsikko {
       font-size: 1.1rem;
@@ -62,6 +251,10 @@ export default class EpTavoitesisaltoalueTavoitealueet extends Vue {
         }
       }
     }
+  }
+
+  .mb-45 {
+    margin-bottom: 2rem;
   }
 
 </style>
