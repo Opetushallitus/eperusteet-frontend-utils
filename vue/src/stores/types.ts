d3ec94744aee5cdbf1865db5a2f37a95b2f2b09c
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-04-09 16:19:25 +0300

    EP-2737

--- vue/src/stores/types.ts
+++ vue/src/stores/types.ts
@@ -1,5 +1,5 @@
 import { Computed } from '@shared/utils/interfaces';
-import { TiedoteDto } from '@shared/api/eperusteet';
+import { PalauteDtoStatusEnum, TiedoteDto } from '@shared/api/eperusteet';
 import { TiedoteQuery } from '@shared/api/types';
 
 export interface ITiedotteetProvider {
@@ -18,6 +18,7 @@ export interface Palaute {
   feedback: string;
   'user_agent': string;
   key?: string,
+  status?: PalauteDtoStatusEnum;
 }
 
 export interface ITPalauteProvider {
@@ -25,4 +26,5 @@ export interface ITPalauteProvider {
   tutkintorakennepalaute: Computed<boolean>;
   fetch?: () => Promise<void>;
   palautteet?: Computed<{[key: string]: Palaute[] | null}>;
+  updatePalaute?: (palaute: Palaute) => Promise<void>;
 }
