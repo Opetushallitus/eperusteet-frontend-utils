544e8aadf6922e3c7ba75c91fc6ccad7164b59ee
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-05-18 14:48:46 +0300

    EP-2797

---
+++ vue/src/components/EpJulkaisuButton/EpJulkaisuButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <ep-button class="mt-3" @click="suoritaJulkaisu()" :showSpinner="julkaistaan" v-oikeustarkastelu="{ oikeus: 'muokkaus' }">
+    {{ $t('julkaise') }}
+  </ep-button>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
+import _ from 'lodash';
+
+@Component({
+  components: {
+    EpButton,
+  },
+})
+export default class EpJulkaisuButton extends Vue {
+  @Prop({ required: true })
+  protected julkaise!: Function;
+
+  private julkaistaan = false;
+
+  async suoritaJulkaisu() {
+    if (await this.$bvModal.msgBoxConfirm((this.$t('julkaisu-varmistus-modal-teksti') as any), {
+      title: this.$t('vahvista-julkaisu'),
+      okVariant: 'primary',
+      okTitle: this.$t('julkaise') as any,
+      cancelVariant: 'link',
+      cancelTitle: this.$t('peruuta') as any,
+      centered: true,
+      ...{} as any,
+    })) {
+      this.julkaistaan = true;
+      await this.julkaise();
+      this.julkaistaan = false;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/_variables.scss';
+
+</style>
