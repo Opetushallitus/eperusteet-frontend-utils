5a17d0fa8f7cb1a1b6d6912d51cc666af4fd5216
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-07-02 11:16:55 +0300

    EP-2869

---
+++ vue/src/utils/perusterakenne.ts
@@ -0,0 +1,47 @@
+export function rakenneNodecolor(node, parentMandatory, el) {
+  const isRyhma = !!node.rooli;
+
+  if (isRyhma) {
+    const mapped = RooliToTheme[node.rooli];
+    if (mapped) {
+      return ColorMap[mapped];
+    }
+    if (node.rooli === 'määritelty') {
+      if (el.$kaanna(node.nimi) === el.$t('rakenne-moduuli-pakollinen')) {
+        return ColorMap.pakollinen;
+      }
+      else if (el.$kaanna(node.nimi) === el.$t('rakenne-moduuli-ammatilliset')) {
+        return ColorMap.ammatilliset;
+      }
+      else if (el.$kaanna(node.nimi) === el.$t('rakenne-moduuli-yhteiset')) {
+        return ColorMap.yhteiset;
+      }
+    }
+    return ColorMap.valinnainen;
+  }
+  else {
+    if (parentMandatory || node.pakollinen) {
+      return ColorMap.pakollinen;
+    }
+    else {
+      return ColorMap.valinnainen;
+    }
+  }
+}
+
+export const RooliToTheme = Object.freeze({
+  'määrittelemätön': 'paikalliset',
+  'osaamisala': 'osaamisala',
+  'tutkintonimike': 'tutkintonimike',
+  'vieras': 'yhteiset',
+});
+
+export const ColorMap = Object.freeze({
+  ammatilliset: '#b2b2b2',
+  osaamisala: '#575757',
+  paikalliset: '#c126b8',
+  pakollinen: '#bdeaa1',
+  tutkintonimike: '#99b3f1',
+  valinnainen: '#e60895',
+  yhteiset: '#878787',
+});
