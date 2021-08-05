2f33c749bfc077db585de59a07bb119ac5b2bf49
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-08-05 17:06:01 +0300

    EP-2859

---
+++ vue/src/components/EpJulkaisuValidointi/IkoniTeksti.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <fas :class="tyyppi" :icon="icon" fixed-width />
+    <span class="pl-2"><slot /></span>
+  </div>
+</template>
+
+<script lang="ts">
+import * as _ from 'lodash';
+import { Prop, Component, Vue, Watch } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class IkoniTeksti extends Vue {
+  @Prop({ required: true })
+  private tyyppi!: 'virhe' | 'huomautus' | 'kaikki-kunnossa';
+
+  get icon() {
+    if (this.tyyppi === 'kaikki-kunnossa') {
+      return 'checkmark-ympyra';
+    }
+
+    return 'huutomerkki-ympyra';
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+
+  .virhe {
+    color: $red;
+  }
+
+  .huomautus {
+    color: $yellow;
+  }
+
+  .kaikki-kunnossa {
+    color: $green;
+  }
+
+</style>
---
+++ vue/src/components/EpJulkaisuValidointi/VirheHuomautukset.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="validointi p-2" v-for="(virhehuomautus, index) in virhehuomautukset" :key="'virheet'+index">
+      <router-link v-if="virhehuomautus.route" :to="virhehuomautus.route">
+        <IkoniTeksti :tyyppi="tyyppi">
+          {{$t(virhehuomautus.kuvaus)}} ({{$kaanna(virhehuomautus.nimi)}})
+        </IkoniTeksti>
+      </router-link>
+      <IkoniTeksti v-else :tyyppi="tyyppi">
+        {{$t(virhehuomautus.kuvaus)}} ({{$kaanna(virhehuomautus.nimi)}})
+      </IkoniTeksti>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import * as _ from 'lodash';
+import { Prop, Component, Vue, Watch } from 'vue-property-decorator';
+import { VirheHuomautus } from './EpJulkaisuValidointi.vue';
+import IkoniTeksti from './IkoniTeksti.vue';
+
+@Component({
+  components: {
+    IkoniTeksti,
+  },
+})
+export default class VirheHuomautukset extends Vue {
+  @Prop({ required: true })
+  private virhehuomautukset!: VirheHuomautus[];
+
+  @Prop({ required: true })
+  private tyyppi!: 'virhe' | 'huomautus';
+}
+</script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+
+  .validointi {
+    &:nth-of-type(even) {
+      background-color: $table-even-row-bg-color;
+    }
+    &:nth-of-type(odd) {
+      background-color: $table-odd-row-bg-color;
+    }
+  }
+
+</style>
