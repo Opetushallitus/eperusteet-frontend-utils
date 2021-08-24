68ac4fc12502be2aa83d2b7ea8a719a2f28c4499
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-08-24 14:22:49 +0300

    pdf icon

--- vue/src/config/fontawesome.ts
+++ vue/src/config/fontawesome.ts
@@ -148,6 +148,7 @@ library.add(oph.opetussuunnitelmasi);
 library.add(oph.osaamisenArviointi);
 library.add(oph.palauta);
 library.add(oph.peruuta);
+library.add(oph.pdflataus);
 library.add(oph.plussa);
 library.add(oph.poistaKolumni);
 library.add(oph.poistaRivi);
