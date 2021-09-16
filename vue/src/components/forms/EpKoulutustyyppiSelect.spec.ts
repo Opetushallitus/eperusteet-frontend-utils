74c34af436430509987ee2f2d5591d6db7fb1359
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-09-16 14:54:24 +0300

    EP-2900

--- vue/src/components/forms/EpKoulutustyyppiSelect.spec.ts
+++ vue/src/components/forms/EpKoulutustyyppiSelect.spec.ts
@@ -19,4 +19,36 @@ describe('EpKoulutustyyppiSelect', () => {
       localVue,
     });
   });
+
+  it('value not set', () => {
+    const wrapper = mount(EpKoulutustyyppiSelect, {
+      propsData: {
+        value: null,
+        isEditing: true,
+      },
+      mocks: {
+        $t: x => x,
+      },
+      localVue,
+    });
+
+    expect(wrapper.find('.multiselect__placeholder').html()).toContain('kaikki');
+    expect(wrapper.html()).not.toContain('.multiselect__single');
+  });
+
+  it('value set', () => {
+    const wrapper = mount(EpKoulutustyyppiSelect, {
+      propsData: {
+        value: 'koulutustyyppi_20',
+        isEditing: true,
+      },
+      mocks: {
+        $t: x => x,
+      },
+      localVue,
+    });
+
+    expect(wrapper.find('.multiselect__single').html()).toContain('koulutustyyppi_20');
+    expect(wrapper.html()).not.toContain('.multiselect__placeholder');
+  });
 });
