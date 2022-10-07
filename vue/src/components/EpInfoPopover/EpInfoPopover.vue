0d5596c2bde89c76be06e0c8ed10f4aa7fd4f815
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-10-07 14:01:19 +0300

    EP-3343

---
+++ vue/src/components/EpInfoPopover/EpInfoPopover.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span class="material-icons-outlined default-icon clickable" id="infopopup">info</span>
+    <b-popover
+      target="infopopup"
+      triggers="hover click blur">
+      <slot/>
+    </b-popover>
+  </div>
+
+</template>
+
+<script lang="ts">
+import * as _ from 'lodash';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class EpInfoPopover extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+
+</style>
