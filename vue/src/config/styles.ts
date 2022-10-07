e0ef7763ae8296234eb7d97c6b22c4bcb8e3ae8c
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-10-07 14:58:09 +0300

    bugfix/EP-3343

--- vue/src/config/styles.ts
+++ vue/src/config/styles.ts
@@ -2,3 +2,4 @@ import '@shared/styles/bootstrap.scss';
 import '@shared/styles/app.scss';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import 'vue-multiselect/dist/vue-multiselect.min.css';
+import 'material-icons/iconfont/material-icons.css';
