a4073c8db6234be43d0cb7eee68fed423b2fe746
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-12-05 13:08:11 +0200

    EP-3447

--- vue/src/utils/koodi.ts
+++ vue/src/utils/koodi.ts
@@ -3,3 +3,28 @@ import { v4 as genUuid } from 'uuid';
 export function generateTemporaryKoodiUri(koodisto: string) {
   return 'temporary_' + koodisto + '_' + genUuid();
 }
+
+export enum Koodistot {
+  AMMATTITAITOVAATIMUKSET = 'ammattitaitovaatimukset',
+  TUTKINNONOSAT = 'tutkinnonosat',
+  TUTKINTO = 'tutkinto',
+  KOULUTUS = 'koulutus',
+  TUTKINTONIMIKKEET = 'tutkintonimikkeet',
+  AMMATILLISENOPPIAINEET = 'ammatillisenoppiaineet',
+  OPPIAINEETJAOPPIMAARATLOPS2021 = 'oppiaineetjaoppimaaratlops2021',
+  LAAJAALAINENOSAAMINENLOPS2021 = 'laajaalainenosaaminenlops2021',
+  LAAJAALAINENOSAAMINENKOTO2022 = 'laajaalainenosaaminenkoto2022',
+  MODUULIKOODISTOLOPS2021 = 'moduulikoodistolops2021',
+  KIELIKOODISTO_OPETUSHALLINTO = 'kielikoodistoopetushallinto',
+  OSAAMISTAVOITTEET = 'osaamistavoitteet',
+  OPINTOKOKONAISUUSTAVOITTEET = 'opintokokonaisuustavoitteet',
+  OPINTOKOKONAISUUSNIMET = 'opintokokonaisuusnimet',
+  ARVIOINTIASTEIKKOAMMATILLINEN = 'arviointiasteikkoammatillinen15',
+  OSAAMISALA = 'osaamisala',
+  TAVOITESISALTOALUEENOTSIKKO = 'tavoitesisaltoalueenotsikko',
+  TAVOITEALUEET = 'tavoitealueet',
+  TAVOITTEETLUKUTAIDOT = 'tavoitteetlukutaidot',
+  KOTOUTUMISKOULUTUSTAVOITTEET = 'kotoutumiskoulutustavoitteet',
+  TUTKINTOKOULUTUKSEEN_VALMENTAVAKOULUTUS_LAAJAALAINENOSAAMINEN = 'tutkintokoulutukseenvalmentavakoulutuslaajaalainenosaaminen',
+  KOULUTUKSENOSATTUVA = 'koulutuksenosattuva',
+}
