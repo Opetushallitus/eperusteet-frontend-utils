ed2877ca1bf2227d8680cda46a65faed4ef6ff63
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-12-15 15:58:19 +0200

    test fix

--- vue/src/utils/__tests__/esikatselu.spec.ts
+++ vue/src/utils/__tests__/esikatselu.spec.ts
@@ -1,4 +1,4 @@
-import { buildEsikatseluUrl } from '../esikatselu';
+import { buildEsikatseluUrl, buildKatseluUrl } from '../esikatselu';
 
 describe('Esikatselu URL', () => {
   function setOrigin(origin) {
@@ -8,44 +8,59 @@ describe('Esikatselu URL', () => {
     } as any;
   }
 
-  test('Localhost linkki', () => {
+  test('Localhost buildEsikatseluUrl', () => {
     const url = buildEsikatseluUrl('fi', '/testi');
-    expect(url).toEqual('http://localhost:9020/#/fi/testi?esikatselu');
+    expect(url).toEqual('http://localhost:9020/#/fi/testi/0');
+  });
+
+  test('Localhost buildKatseluUrl postpath', () => {
+    const url = buildKatseluUrl('fi', '/testi', 1, '/testi');
+    expect(url).toEqual('http://localhost:9020/#/fi/testi/1/testi');
+  });
+
+  test('Localhost buildKatseluUrl', () => {
+    const url = buildKatseluUrl('fi', '/testi', 1);
+    expect(url).toEqual('http://localhost:9020/#/fi/testi/1');
+  });
+
+  test('Localhost buildKatseluUrl', () => {
+    const url = buildKatseluUrl('fi', '/testi');
+    expect(url).toEqual('http://localhost:9020/#/fi/testi');
   });
 
   test('Production linkki', () => {
     setOrigin('https://virkailija.opintopolku.fi');
     const url = buildEsikatseluUrl('fi', '/testi');
-    expect(url).toEqual('https://eperusteet.opintopolku.fi/#/fi/testi?esikatselu');
+    expect(url).toEqual('https://eperusteet.opintopolku.fi/#/fi/testi/0');
   });
 
   test('QA linkki', () => {
     setOrigin('https://virkailija.testiopintopolku.fi');
-    const url = buildEsikatseluUrl('fi', '/testi');
-    expect(url).toEqual('https://eperusteet.testiopintopolku.fi/#/fi/testi?esikatselu');
+    const url = buildEsikatseluUrl('fi', '/testi', '/testi');
+    expect(url).toEqual('https://eperusteet.testiopintopolku.fi/#/fi/testi/0/testi');
   });
 
   test('Hahtuva linkki', () => {
     setOrigin('https://virkailija.hahtuvaopintopolku.fi');
     const url = buildEsikatseluUrl('fi', '/testi');
-    expect(url).toEqual('https://eperusteet.hahtuvaopintopolku.fi/#/fi/testi?esikatselu');
+    expect(url).toEqual('https://eperusteet.hahtuvaopintopolku.fi/#/fi/testi/0');
   });
 
   test('Untuva linkki', () => {
     setOrigin('https://virkailija.untuvaopintopolku.fi');
     const url = buildEsikatseluUrl('fi', '/testi');
-    expect(url).toEqual('https://eperusteet.untuvaopintopolku.fi/#/fi/testi?esikatselu');
+    expect(url).toEqual('https://eperusteet.untuvaopintopolku.fi/#/fi/testi/0');
   });
 
   test('Kieli linkki', () => {
     setOrigin('https://virkailija.opintopolku.fi');
     const url = buildEsikatseluUrl('sv', '/testi');
-    expect(url).toEqual('https://eperusteet.opintopolku.fi/#/sv/testi?esikatselu');
+    expect(url).toEqual('https://eperusteet.opintopolku.fi/#/sv/testi/0');
   });
 
   test('Esikatselu linkki', () => {
     setOrigin('https://virkailija.opintopolku.fi');
     const url = buildEsikatseluUrl('fi', '/opetussuunnitelma/1/lukiokoulutus/tiedot');
-    expect(url).toEqual('https://eperusteet.opintopolku.fi/#/fi/opetussuunnitelma/1/lukiokoulutus/tiedot?esikatselu');
+    expect(url).toEqual('https://eperusteet.opintopolku.fi/#/fi/opetussuunnitelma/1/lukiokoulutus/tiedot/0');
   });
 });
