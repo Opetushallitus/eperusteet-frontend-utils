34ea1a600a3703468e0db55b87e26c76082f43cf
Timo Paasila <timo.paasila@gofore.com>
2023-01-09 09:35:24 +0200

    EP-3510

---
+++ vue/src/components/EpErrorPage/EpErrorPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container my-5">
+    <div class="px-3 px-md-0">
+      <h2>{{ $t('virhe-nakyma-otsikko') }}</h2>
+      <h3>{{ virheIlmoitus }}</h3>
+      <div class="virhekuva">
+        <img :src="virhekuva" :alt="$t('virhe-kuva-teksti')">
+      </div>
+
+      <div class="d-flex flex-row-reverse">
+        <div class="align-self-center">
+          <router-link :to="{ name: 'root'}">{{ $t('palaa-etusivulle') }}</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" >
+import { Component, Vue } from 'vue-property-decorator';
+import { createLogger } from '@shared/utils/logger';
+import EpField from '@shared/components/forms/EpField.vue';
+import EpFormContent from '@shared/components/forms/EpFormContent.vue';
+
+const virhekuva = require('@assets/img/images/virhe.png');
+
+const logger = createLogger('Virhe');
+
+@Component({
+  components: {
+    EpField,
+    EpFormContent,
+  },
+})
+export default class EpErrorPage extends Vue {
+  get virheIlmoitus() {
+    return this.$route.query && this.$route.query.errorMessage ? this.$route.query.errorMessage : null;
+  }
+
+  get virhekuva() {
+    return virhekuva;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@shared/styles/_variables.scss";
+
+.virhekuva {
+  img {
+    width: 100%;
+  }
+  padding: 0 10% 0 10%;
+}
+</style>
