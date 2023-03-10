b2f03bb3d99812c4c226dd51f94e0a10568d30d9
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-03-10 12:57:21 +0200

    tutkinnonosa components

---
+++ vue/src/components/EpTutkinnonosa/Arviointi2020Taulukko.spec.ts
@@ -0,0 +1,47 @@
+import BootstrapVue from 'bootstrap-vue';
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import VueRouter from 'vue-router';
+import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils';
+import Arviointi2020Taulukko from './Arviointi2020Taulukko.vue';
+
+Vue.use(BootstrapVue);
+
+describe('Arviointi 2020', () => {
+  const localVue = createLocalVue();
+  localVue.use(VueI18n);
+  localVue.use(VueRouter);
+
+  test('Renders', async () => {
+    const wrapper = mount(Arviointi2020Taulukko, {
+      localVue,
+      propsData: {
+        arviointi: {
+          osaamistasonKriteerit: [{
+            osaamistaso: {
+              otsikko: {
+                fi: 'otsikko',
+              },
+            },
+            kriteerit: [{
+              fi: 'kriteeri',
+            }],
+          }],
+        },
+      },
+      mocks: {
+        $t: x => x,
+        $kaanna: x => x && x.fi,
+      },
+      stubs: {
+        fas: '<div />',
+        PortalTarget: '<div />',
+        'router-link': RouterLinkStub,
+      },
+    });
+
+    expect(wrapper.html()).toBeTruthy();
+    expect(wrapper.html()).toContain('otsikko');
+    expect(wrapper.html()).toContain('kriteeri');
+  });
+});
---
+++ vue/src/components/EpTutkinnonosa/Arviointi2020Taulukko.vue
@@ -0,0 +1,65 @@
+<template>
+  <ep-form-content class="col-md-12 mb-5" v-if="arviointi">
+    <slot name="header">
+    </slot>
+    <div>{{$kaanna(arviointi.kohde)}}</div>
+    <b-container fluid="lg" class="osaamistasot mt-3">
+      <b-row v-for="(osaamistaso, index) in arviointi.osaamistasonKriteerit" :key="'osaamistasokriteeri'+index">
+        <b-col class="pt-3" md="12" lg="4">
+          <span>{{$kaanna(osaamistaso.osaamistaso.otsikko)}}</span>
+        </b-col>
+        <b-col class="pt-3" md="12" lg="8">
+          <ul class="pl-3">
+            <li v-for="(kriteeri, index) in osaamistaso.kriteerit" :key="'kriteeri'+index">
+              {{$kaanna(kriteeri)}}
+            </li>
+          </ul>
+        </b-col>
+      </b-row>
+    </b-container>
+  </ep-form-content>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import EpFormContent from '@shared/components/forms/EpFormContent.vue';
+import EpAlert from '@shared/components/EpAlert/EpAlert.vue';
+import * as _ from 'lodash';
+
+@Component({
+  components: {
+    EpFormContent,
+    EpAlert,
+  },
+})
+export default class Arviointi2020Taulukko extends Vue {
+  @Prop({ required: true })
+  private arviointi!: any;
+
+  get osaamistasonKriteeritFields() {
+    return [{
+      key: 'osaamistaso',
+      label: this.$t('osaamistaso') as string,
+      thStyle: { display: 'none' },
+    }, {
+      key: 'kriteerit',
+      label: this.$t('kriteerit') as string,
+      thStyle: { display: 'none' },
+    }] as any[];
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@shared/styles/_variables.scss";
+
+  .osaamistasot {
+    .row:nth-of-type(even) {
+      background-color: $table-even-row-bg-color;
+    }
+    .row:nth-of-type(odd) {
+      background-color: $table-odd-row-bg-color;
+    }
+  }
+
+</style>
