43a9a46e38cf930b6fec7f0c858c81999475cf12
timopaasila <113356513+timopaasila@users.noreply.github.com>
2023-03-24 14:36:51 +0200

    EP-3645 (#720)
    EP-3645

--- vue/src/components/EpJulkaisuHistoriaJulkinen/EpJulkaisuLista.vue
+++ vue/src/components/EpJulkaisuHistoriaJulkinen/EpJulkaisuLista.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="mt-2">
-    <div v-for="(julkaisu, index) in julkaisut" :key="'julkaisu'+index" class="julkaisu pb-2 pt-2 ml-1 px-3">
+    <div v-for="(julkaisu, index) in julkaisutFiltered" :key="'julkaisu'+index" class="julkaisu pb-2 pt-2 ml-1 px-3">
       <div class="d-flex">
         <span class="font-bold font-size pr-3 ">{{$t('julkaisu')}} {{ $sd(julkaisu.luotu) }}</span>
-        <span v-if="latestJulkaisuRevision && latestJulkaisuRevision.revision === julkaisu.revision" class="julkaistu">{{$t('uusin-versio')}}</span>
+        <span v-if="latestJulkaisuRevision && latestJulkaisuRevision.revision === julkaisu.revision" class="julkaistu">{{$t('uusin')}}</span>
       </div>
       <div v-if="julkaisu.muutosmaaraysVoimaan && julkaisu.liitteet && julkaisu.liitteet.length > 0" class="mt-2">
         <div v-for="(liiteData, index) in julkaisu.liitteet" :key="'maarays'+index" class="maarayslinkit">
-          <a :href="liiteData.url" target="_blank" rel="noopener noreferrer">{{ liiteData.nimi }} ({{ $t(liiteData.kieli) }})</a>
+          <a :href="liiteData.url"
+             target="_blank"
+             rel="noopener noreferrer">{{ liiteData.nimi }}</a>
         </div>
-        <span>- {{ $sd(julkaisu.muutosmaaraysVoimaan) }} {{ $t('alkaen') }}</span>
+        <span>- {{ $t('voimassaolo-alkaa') }} {{ $sd(julkaisu.muutosmaaraysVoimaan) }}</span>
       </div>
       <div v-if="julkaisu.julkinenTiedote" class="my-1" v-html="$kaanna(julkaisu.julkinenTiedote)"></div>
     </div>
@@ -18,6 +20,8 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import _ from 'lodash';
+import { Kielet } from '@shared/stores/kieli';
 
 @Component({
   components: {
@@ -29,6 +33,17 @@ export default class EpJulkaisuLista extends Vue {
 
   @Prop({ required: true })
   private latestJulkaisuRevision!: any;
+
+  get julkaisutFiltered() {
+    return _.chain(this.julkaisut)
+      .map(julkaisu => {
+        return {
+          ...julkaisu,
+          liitteet: _.filter(julkaisu.liitteet, liite => liite.kieli === Kielet.getSisaltoKieli.value),
+        };
+      })
+      .value();
+  }
 }
 </script>
 
