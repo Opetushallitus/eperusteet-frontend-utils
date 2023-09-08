3bd8bd034da3e1c81a16b60f468911c2952daa15
Timo Paasila <timo.paasila@gofore.com>
2023-09-08 10:49:58 +0300

    EP-3858

--- vue/src/components/EpVoimassaoloFilter/EpVoimassaoloFilter.vue
+++ vue/src/components/EpVoimassaoloFilter/EpVoimassaoloFilter.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="d-flex align-self-center flex-wrap flex-lg-row flex-column toggles">
-    <EpToggle v-model="query.tuleva" class="haku-toggle tuleva" :is-s-witch="false">
-      <span v-if="query.tuleva" class="sr-only">{{ $t('valittu') }}</span>
+    <EpToggle v-model="queryData.tuleva" class="haku-toggle" :is-s-witch="false">
+      <span v-if="queryData.tuleva" class="sr-only">{{ $t('valittu') }}</span>
       <span class="sr-only">{{ $t('voimassaolo-filtteri') }}</span>
       {{ $t('tulossa-voimaan') }}
     </EpToggle>
-    <EpToggle v-model="query.voimassaolo" class="haku-toggle voimassaolo" :is-s-witch="false">
-      <span v-if="query.voimassaolo" class="sr-only">{{ $t('valittu') }}</span>
+    <EpToggle v-model="queryData.voimassaolo" class="haku-toggle" :is-s-witch="false">
+      <span v-if="queryData.voimassaolo" class="sr-only">{{ $t('valittu') }}</span>
       <span class="sr-only">{{ $t('voimassaolo-filtteri') }}</span>
       {{ $t('voimassa') }}
     </EpToggle>
-    <EpToggle v-model="query.poistunut" class="haku-toggle poistunut" :is-s-witch="false">
-      <span v-if="query.poistunut" class="sr-only">{{ $t('valittu') }}</span>
+    <EpToggle v-if="queryData.siirtyma != null" v-model="queryData.siirtyma" class="haku-toggle" :is-s-witch="false">
+      <span v-if="queryData.siirtyma" class="sr-only">{{ $t('valittu') }}</span>
+      <span class="sr-only">{{ $t('voimassaolo-filtteri') }}</span>
+      {{ $t('ajoitus-siirtyma') }}
+    </EpToggle>
+    <EpToggle v-model="queryData.poistunut" class="haku-toggle" :is-s-witch="false">
+      <span v-if="queryData.poistunut" class="sr-only">{{ $t('valittu') }}</span>
       <span class="sr-only">{{ $t('voimassaolo-filtteri') }}</span>
       {{ $t('ei-voimassa') }}
     </EpToggle>
@@ -19,7 +24,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import EpToggle from '@shared/components/forms/EpToggle.vue';
 
 @Component({
@@ -30,6 +35,15 @@ import EpToggle from '@shared/components/forms/EpToggle.vue';
 export default class EpVoimassaoloFilter extends Vue {
   @Prop({ required: true })
   private query!: any;
+
+  get queryData() {
+    return this.query;
+  }
+
+  @Watch('queryData', { deep: true })
+  onQueryChanged() {
+    this.$emit('voimassaoloFilterChange', this.query);
+  }
 }
 </script>
 
@@ -45,22 +59,6 @@ export default class EpVoimassaoloFilter extends Vue {
       border: 1px solid $gray-lighten-8;
       border-radius: 6px;
       margin-right: 7px;
-
-      ::v-deep .toggle {
-        border: 0;
-      }
-
-      &.tuleva ::v-deep .toggle {
-         background-color: $tuleva-color;
-      }
-
-      &.voimassaolo ::v-deep .toggle {
-         background-color: $green;
-      }
-
-      &.poistunut ::v-deep .toggle {
-         background-color: $paattynyt-color;
-      }
     }
   }
 </style>
