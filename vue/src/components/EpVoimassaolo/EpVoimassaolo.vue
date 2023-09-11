f5d22b30eb667ee511d4f294835a0768710d315a
timopaasila <113356513+timopaasila@users.noreply.github.com>
2023-09-11 10:31:18 +0300

    Ep 3858 (#804)
    EP-3858

--- vue/src/components/EpVoimassaolo/EpVoimassaolo.vue
+++ vue/src/components/EpVoimassaolo/EpVoimassaolo.vue
@@ -1,15 +1,19 @@
 <template>
-  <span v-if="eraantynyt" class="ml-3">
-    <EpColorIndicator :size="10" background-color="#FF5000" :tooltip="false" kind=""/>
-    {{ $t('ei-voimassa') }}
-  </span>
-  <span v-else-if="voimassa" class="ml-3">
+  <span v-if="voimassa" class="ml-2">
     <EpColorIndicator :size="10" background-color="#4c7f00" :tooltip="false" kind=""/>
     {{ $t('voimassa') }}
   </span>
-  <span v-else-if="tulossa" class="ml-3">
+  <span v-else-if="tulossa" class="ml-2">
     <EpColorIndicator :size="10" background-color="#5BCA13" :tooltip="false" kind=""/>
     {{ $t('tulossa-voimaan') }}
+  </span>
+  <span v-else-if="siirtymaAjalla" class="ml-2">
+    <EpColorIndicator :size="10" background-color="#F5B800" :tooltip="false" kind=""/>
+    {{ $t('ajoitus-siirtyma') }}
+  </span>
+  <span v-else-if="eraantynyt" class="ml-2">
+    <EpColorIndicator :size="10" background-color="#FF5000" :tooltip="false" kind=""/>
+    {{ $t('ei-voimassa') }}
   </span>
 </template>
 
@@ -21,6 +25,7 @@ interface Voimassaolo {
   voimaantulo?: number;
   voimassaoloAlkaa?: number;
   voimassaoloLoppuu?: number;
+  siirtymaPaattyy?: number;
 }
 
 @Component({
@@ -46,6 +51,10 @@ export default class EpVoimassaolo extends Vue {
 
   get tulossa() {
     return this.voimassaolonAlku && this.voimassaolonAlku > Date.now();
+  }
+
+  get siirtymaAjalla() {
+    return this.voimassaolo.siirtymaPaattyy && Date.now() < this.voimassaolo.siirtymaPaattyy;
   }
 }
 </script>
