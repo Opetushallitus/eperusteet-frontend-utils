850a5287a78012fdc196f44e455fae2445e96bb0
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-12-27 16:28:37 +0200

    EP-4047

---
+++ vue/src/config/defaultcomponents.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import EpMaterialIcon from '@shared/components/EpMaterialIcon/EpMaterialIcon.vue';
+import EpToggle from '@shared/components/forms/EpToggle.vue';
+
+Vue.component('EpMaterialIcon', EpMaterialIcon);
+Vue.component('EpToggle', EpToggle);
