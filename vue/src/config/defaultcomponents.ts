9ec11845fafbc84ad906ed0a5ecef456d825c0c8
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-03-04 16:07:38 +0200

    EP-4117

--- vue/src/config/defaultcomponents.ts
+++ vue/src/config/defaultcomponents.ts
@@ -5,6 +5,8 @@ import EpPdfLink from '@shared/components/EpPdfLink/EpPdfLink.vue';
 import EpDatepicker from '@shared/components/forms/EpDatepicker.vue';
 import EpMultiSelect from '@shared/components/forms/EpMultiSelect.vue';
 import EpFormContent from '@shared/components/forms/EpFormContent.vue';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
+import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
 
 Vue.component('EpMaterialIcon', EpMaterialIcon);
 Vue.component('EpToggle', EpToggle);
@@ -12,3 +14,5 @@ Vue.component('EpPdfLink', EpPdfLink);
 Vue.component('EpDatepicker', EpDatepicker);
 Vue.component('EpMultiSelect', EpMultiSelect);
 Vue.component('EpFormContent', EpFormContent);
+Vue.component('EpButton', EpButton);
+Vue.component('EpSpinner', EpSpinner);
