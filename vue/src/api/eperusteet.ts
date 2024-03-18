eabc349caec62f6c5168a51e456cd10c4a4322e9
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-03-18 16:20:39 +0200

    EP-4152

--- vue/src/api/eperusteet.ts
+++ vue/src/api/eperusteet.ts
@@ -32,6 +32,7 @@ export import MaaraysDtoTilaEnum = EperusteetApi.MaaraysDtoTilaEnum;
 export import MaaraysKevytDtoTilaEnum = EperusteetApi.MaaraysDtoTilaEnum;
 export import MaaraysLiiteDtoTyyppiEnum = EperusteetApi.MaaraysLiiteDtoTyyppiEnum;
 export import JulkiEtusivuDtoEtusivuTyyppiEnum = EperusteetApi.JulkiEtusivuDtoEtusivuTyyppiEnum;
+export import PerusteBaseDtoOpasTyyppiEnum = EperusteetApi.PerusteBaseDtoOpasTyyppiEnum;
 
 const logger = createLogger('EperusteetAxios');
 const basePath = '';
