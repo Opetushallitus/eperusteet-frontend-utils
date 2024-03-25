f33f084fa3009967b8993d40d12d104f3c5f18c2
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-03-25 10:42:42 +0200

    EP-4171

---
+++ vue/src/components/EpInfoBanner/EpInfoBanner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ep-info-banner d-flex p-3">
+    <EpMaterialIcon class="icon mr-2">info</EpMaterialIcon>
+    <div><slot/></div>
+  </div>
+</template>
+
+<script lang="ts">
+import * as _ from 'lodash';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class EpInfoBanner extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+
+ .ep-info-banner {
+  background-color: $blue-lighten-4;
+  border-radius: 5px;
+ }
+
+ .icon {
+  color: $gray-lighten-12;
+ }
+
+</style>
