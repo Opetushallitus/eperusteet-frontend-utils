99bff0ac8483288e2ad260257b6fc34cb8c4365c
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-09-03 13:55:25 +0300

    EP-4261-5

---
+++ vue/src/components/EpPaikallinenTarkennus/EpPaikallinenTarkennus.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="paikallinen-tarkennus-alue">
+    <div class="otsikko">{{ $t('paikallinen-teksti') }}</div>
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import * as _ from 'lodash';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class EpPaikallinenTarkennus extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+
+.paikallinen-tarkennus-alue {
+  border-radius: 1rem;
+  background-color: $ylops-paikallinen-color;
+  padding: 0.8rem;
+
+  .otsikko {
+    font-weight: 600;
+    margin-bottom: 8px;
+  }
+}
+
+</style>
