d85fd340da414ecd533362d1e456f6a8324fdde0
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2024-09-09 09:09:24 +0300

    Ep 4326 (#998)
    * EP-4326
    
    * label fix
    
    * amosaa changes
    
    * amosaa lisäykset

---
+++ vue/src/components/EpEsikatselu/EpEsikatselu.vue
@@ -0,0 +1,117 @@
+<template>
+  <div >
+    <label class="font-weight-600">
+      <slot name="header">
+        {{$t(header)}}
+      </slot>
+    </label>
+    <ep-toggle v-model="model.esikatseltavissa" :is-editing="isEditing" v-if="isEditing" :class="{'disabled-events': model.tila === 'poistettu'}">
+      <slot name="toggle-text">
+        {{$t(toggleText)}}
+      </slot>
+    </ep-toggle>
+    <ep-external-link :url="externalUrl" v-if="!isEditing && model.esikatseltavissa"/>
+    <div v-if="!isEditing && !model.esikatseltavissa">
+      {{$t('et-ole-sallinut-esikatselua')}}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import * as _ from 'lodash';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Kielet } from '@shared/stores/kieli';
+import { PerusteDto } from '@shared/api/eperusteet';
+import { koulutustyyppiTheme } from '@shared/utils/perusteet';
+import { buildPerusteEsikatseluUrl, buildEsikatseluUrl, buildToteutussuunnitelmaEsikatseluUrl } from '@shared/utils/esikatselu';
+
+interface Esikatseltavissa {
+  id: number;
+  esikatseltavissa: boolean;
+  koulutustyyppi: string;
+  tyyppi: string;
+  tila: string;
+  jotpatyyppi?: string;
+  peruste?: PerusteDto
+}
+
+@Component
+export default class EpEsikatselu extends Vue {
+  @Prop()
+  private value!: Esikatseltavissa;
+
+  @Prop({ default: false })
+  private isEditing!: Boolean;
+
+  @Prop({ default: false, type: Boolean })
+  private peruste!: Boolean;
+
+  @Prop({ default: false, type: Boolean })
+  private opetussuunnitelma!: Boolean;
+
+  @Prop({ default: false, type: Boolean })
+  private toteutussuunnitelma!: Boolean;
+
+  @Prop({ default: false, type: Boolean })
+  private opas!: Boolean;
+
+  get model() {
+    return this.value;
+  }
+
+  set model(value) {
+    this.$emit('input', value);
+  }
+
+  get type() {
+    if (this.peruste) {
+      return 'peruste';
+    }
+    else if (this.opetussuunnitelma) {
+      return 'opetussuunnitelma';
+    }
+    else if (this.toteutussuunnitelma) {
+      return 'toteutussuunnitelma';
+    }
+    else if (this.opas) {
+      return 'opas';
+    }
+    return 'peruste';
+  }
+
+  get header() {
+    return 'esikatsele-' + this.type;
+  }
+
+  get toggleText() {
+    return 'salli-esikatselu-' + this.type;
+  }
+
+  get externalUrl() {
+    if (this.peruste) {
+      return buildPerusteEsikatseluUrl(this.value.peruste);
+    }
+
+    if (this.opas) {
+      return buildEsikatseluUrl(Kielet.getSisaltoKieli.value, `/opas/${this.value.id}`);
+    }
+
+    if (this.opetussuunnitelma) {
+      return buildEsikatseluUrl(Kielet.getSisaltoKieli.value, `/opetussuunnitelma/${this.value.id}`, `/${koulutustyyppiTheme(this.value.koulutustyyppi!)}/tiedot`);
+    }
+
+    if (this.toteutussuunnitelma) {
+      return buildToteutussuunnitelmaEsikatseluUrl(this.value, this.amosaaToteutustyyppi);
+    }
+  }
+
+  get amosaaToteutustyyppi() {
+    return (this.$route as any).params.toteutus;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@shared/styles/_variables.scss';
+
+</style>
