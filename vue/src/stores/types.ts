874acb359d12cee9b188663b10942da7495dd9d9
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2024-10-01 13:20:06 +0300

    Ep 4364 (#1010)
    * EP-4364
    
    * ylops
    
    * amosaa

--- vue/src/stores/types.ts
+++ vue/src/stores/types.ts
@@ -1,5 +1,5 @@
 import { Computed } from '@shared/utils/interfaces';
-import { PalauteDtoStatusEnum, TiedoteDto } from '@shared/api/eperusteet';
+import { TiedoteDto } from '@shared/api/eperusteet';
 import { TiedoteQuery } from '@shared/api/types';
 
 export interface ITiedotteetProvider {
@@ -11,20 +11,4 @@ export interface ITiedotteetProvider {
   fetch: () => Promise<void>;
   save?: (tiedote: TiedoteDto) => Promise<void>;
   delete?: (tiedote: TiedoteDto) => Promise<void>;
-}
-
-export interface Palaute {
-  stars: number;
-  feedback: string;
-  'user_agent': string;
-  key?: string,
-  status?: PalauteDtoStatusEnum;
-}
-
-export interface ITPalauteProvider {
-  sendPalaute: (palaute: Palaute) => Promise<void>;
-  tutkintorakennepalaute: Computed<boolean>;
-  fetch?: () => Promise<void>;
-  palautteet?: Computed<{[key: string]: Palaute[] | null}>;
-  updatePalaute?: (palaute: Palaute) => Promise<void>;
 }
