f3bb59595edc843be81b4f0528378db799e35ae3
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-01-27 15:45:41 +0200

    EP-4600

--- components/injector/injector.ts
+++
@@ -1,9 +0,0 @@
-function inject(injector, injectables: Array<any>) {
-    let strs = _.clone(injectables);
-    let obj: any = {};
-    injectables.push(function() {
-        _.each(arguments, (arg, idx) => obj[strs[idx]] = arg);
-    });
-    injector.invoke(injectables);
-    return obj;
-}
