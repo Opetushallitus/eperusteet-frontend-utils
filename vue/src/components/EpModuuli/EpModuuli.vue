4df4a8b5ac19540b25ac5095c2d1295e1e42596b
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2025-03-17 15:10:18 +0200

    Merge pull request #1082 from Opetushallitus/EP-4698
    Ep 4698

--- vue/src/components/EpModuuli/EpModuuli.vue
+++ vue/src/components/EpModuuli/EpModuuli.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="moduuli d-flex flex-column align-items-end px-2 py-3 m-2">
-      <div class="font-weight-bold">{{$kaanna(moduuli.nimi)}} <span v-if="moduuli.koodi">({{moduuli.koodi.arvo}})</span></div>
-      <div class="d-flex justify-content-end mt-auto">
-        <div class="mr-1 opintopiste" v-if="moduuli.laajuus">{{moduuli.laajuus}} {{$t('opintopiste')}}</div>
-        <EpColorIndicator class="mt-1" :kind="moduuli.pakollinen ? 'pakollinen' : 'valinnainen'"/>
-      </div>
+  <div class="moduuli d-flex justify-content-between p-2">
+    <div class="font-weight-bold">{{$kaanna(moduuli.nimi)}} <span v-if="moduuli.koodi">({{moduuli.koodi.arvo}})</span></div>
+    <div class="d-flex align-items-center">
+      <div class="pr-2 opintopiste" v-if="moduuli.laajuus">{{moduuli.laajuus}} {{$t('opintopiste')}}</div>
+      <EpColorIndicator class="mt-1" :kind="moduuli.pakollinen ? 'pakollinen' : 'valinnainen'"/>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import * as _ from 'lodash';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import EpColorIndicator from '@shared/components/EpColorIndicator/EpColorIndicator.vue';
-import { LokalisoituTekstiDto } from '@shared/tyypit';
 import { KoodiDto } from '@shared/api/eperusteet';
 
 interface Moduuli {
@@ -36,11 +35,13 @@ export default class EpModuuli extends Vue {
 <style scoped lang="scss">
 @import '@shared/styles/_variables.scss';
   .moduuli {
-    width:160px;
-    height:160px;
     background-color: #eaf6fe;
-    border-radius: 0.5em;
+    border-radius: 5px;
     color: $blue-darken-1;
+
+    &:hover {
+      background-color: #C3EAFF;
+    }
 
     .opintopiste {
       font-size: 0.85rem;
