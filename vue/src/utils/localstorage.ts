bb7fdfcfb6cebdf4311068ad58b60ca5f1b80e0c
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2025-06-13 13:36:49 +0300

    EP-4757 (#1100)

--- vue/src/utils/localstorage.ts
+++ vue/src/utils/localstorage.ts
@@ -3,23 +3,27 @@ import { createLogger } from './logger';
 const logger = createLogger('LocalStorage');
 
 export function removeItem(key: string) {
-  logger.debug('Removing item', key);
-  localStorage.removeItem(key);
+  logger.debug('Removing item', normalizeKey(key));
+  localStorage.removeItem(normalizeKey(key));
 }
 
 export function setItem<T>(key: string, value: T) {
-  logger.debug('Setting item', key, value);
-  localStorage.setItem(key, JSON.stringify(value));
+  logger.debug('Setting item', normalizeKey(key), value);
+  localStorage.setItem(normalizeKey(key), JSON.stringify(value));
 }
 
 export function getItem<T>(key: string): T | null {
-  const value = localStorage.getItem(key);
+  const value = localStorage.getItem(normalizeKey(key));
   if (value) {
     const parsed = JSON.parse(value);
-    logger.debug('Getting item', key, parsed);
+    logger.debug('Getting item', normalizeKey(key), parsed);
     return parsed;
   }
   else {
     return null;
   }
 }
+
+const normalizeKey = (key: string): string => {
+  return 'eperusteet-' + key;
+};
