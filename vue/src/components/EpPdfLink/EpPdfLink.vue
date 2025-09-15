5569d49a6d754718477db0ebb3e0476d5416cfa9
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-09-15 16:13:20 +0300

    fix

--- vue/src/components/EpPdfLink/EpPdfLink.vue
+++ vue/src/components/EpPdfLink/EpPdfLink.vue
@@ -16,6 +16,7 @@
 </template>
 
 <script setup lang="ts">
+import EpMaterialIcon from '@shared/components/EpMaterialIcon/EpMaterialIcon.vue';
 const props = defineProps({
   url: {
     type: String,
