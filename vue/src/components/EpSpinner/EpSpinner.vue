7e7b362e31dba8b9845c05abc2e60cc1dd762a1d
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2026-01-19 11:12:34 +0200

    EP-4922

--- vue/src/components/EpSpinner/EpSpinner.vue
+++ vue/src/components/EpSpinner/EpSpinner.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="spinner"
-    :class="{'small': small}"
+    :class="{'small': small, 'full-screen': fullScreen}"
   >
     <div class="oph-spinner">
       <div
@@ -33,6 +33,11 @@ const props = defineProps({
     type: String,
     required: false,
   },
+  fullScreen: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
 });
 
 const style = computed(() => {
@@ -59,6 +64,23 @@ const style = computed(() => {
 
   .oph-bounce {
     background-color: $spinner-background-color !important;
+  }
+
+  &.full-screen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.8);
+    z-index: 1000;
+
+    .oph-spinner {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
 }
 </style>
