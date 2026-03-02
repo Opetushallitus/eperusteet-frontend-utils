681fbf86446225d34383daaee04ef0d636df47c9
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2026-03-02 16:29:50 +0200

    peruste fixes

--- vue/src/components/EpInputGroup/EpInputGroup.vue
+++ vue/src/components/EpInputGroup/EpInputGroup.vue
@@ -1,7 +1,12 @@
 <template>
-  <InputGroup class="ep-input-group">
-    <slot />
-    <InputGroupAddon v-if="$slots.append">
+  <InputGroup class="ep-input-group" :class="{ 'disabled': disabled }">
+    <InputGroupAddon v-if="$slots.prefix">
+      <slot name="prefix" />
+    </InputGroupAddon>
+    <div class="flex-grow middle-input" :class="{ 'with-prefix': $slots.prefix, 'with-append': $slots.append }">
+      <slot />
+    </div>
+    <InputGroupAddon v-if="$slots.append" class="append-addon">
       <slot name="append" />
     </InputGroupAddon>
   </InputGroup>
@@ -10,10 +15,49 @@
 <script setup lang="ts">
 import InputGroup from 'primevue/inputgroup';
 import InputGroupAddon from 'primevue/inputgroupaddon';
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 
-<style scoped lang="scss">
-.ep-input-group {
-  display: flex;
+<style lang="scss" scoped>
+@import '@shared/styles/_variables.scss';
+
+.disabled {
+  :deep(input) {
+    background-color: $grey200 !important;
+  }
+  :deep(.p-inputgroupaddon) {
+    background-color: $grey200 !important;
+  }
+
+  .middle-input {
+    background-color: $grey200 !important;
+  }
+}
+
+.middle-input {
+
+  border: 1px solid #ccc;
+
+  &.with-prefix {
+    border-left: 0 !important;
+  }
+  &.with-append {
+    border-right: 0 !important;
+  }
+  :deep(input) {
+    border: 0 !important;
+  }
+}
+.append-addon {
+  :deep(button) {
+    border-top-left-radius: 0 !important;
+    border-bottom-left-radius: 0 !important;
+  }
 }
 </style>
