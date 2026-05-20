4348008c9b4ea81fa1106471f48a75321b57cec5
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2026-05-20 10:18:41 +0300

    accessibility fixes

--- vue/src/config/styles.ts
+++ vue/src/config/styles.ts
@@ -16,3 +16,4 @@ import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
 import '@fontsource/poppins/800.css';
+import '@shared/styles/accessibility.scss';
