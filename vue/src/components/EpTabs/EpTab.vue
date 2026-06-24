d0f058ca27e379a38c7eed8a259fbe6597079c39
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2026-06-24 12:59:15 +0300

    tabs

--- vue/src/components/EpTabs/EpTab.vue
+++ vue/src/components/EpTabs/EpTab.vue
@@ -1,29 +1,7 @@
-<template>
-  <div class="ep-tab-content">
-    <slot />
-  </div>
-</template>
-
 <script setup lang="ts">
-defineOptions({
-  name: 'EpTab',
-});
-
-defineProps({
-  title: {
-    type: String,
-    required: true,
-  },
-  tabIndex: {
-    type: Number,
-    default: 0,
-  },
-});
+defineProps<{
+  title: string;
+}>();
 </script>
 
-<style scoped lang="scss">
-.ep-tab-content {
-  margin-top: 1rem;
-}
-</style>
-
+<template />
