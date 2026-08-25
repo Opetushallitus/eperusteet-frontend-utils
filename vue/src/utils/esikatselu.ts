9142d92739be55158f05e4d6e99b72dbab69b8ee
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2026-08-25 12:43:14 +0300

    EP-5125

--- vue/src/utils/esikatselu.ts
+++ vue/src/utils/esikatselu.ts
@@ -57,7 +57,7 @@ export function buildToteutussuunnitelmaEsikatseluUrl(toteutussuunnitelma, toteu
   return buildEsikatseluUrl(Kielet.getSisaltoKieli.value, `/toteutussuunnitelma/${toteutussuunnitelma.id}`, `/${esikatselutoteutus(toteutussuunnitelma, toteutus)}`);
 }
 
-function esikatselutoteutus(toteutussuunnitelma, toteutus) {
+export function esikatselutoteutus(toteutussuunnitelma, toteutus) {
   if (toteutussuunnitelma.jotpatyyppi === 'MUU') {
     return 'muukoulutus';
   }
